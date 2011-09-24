41ec855a17dd8cdfa68e5ca18620f3a8eb331ce9
Sleepcap <Oliver.Auth@rhoen.de>
2011-09-24 22:27:07 +0200

    git-test

--- board.php
+++ board.php
@@ -363,7 +363,7 @@ if($User->type['Moderator'])
 			$modActions[] = '<br />Multi-check:';
 			foreach($Game->Members->ByCountryID as $countryID=>$Member)
 			{
-				$modActions[] = '<a href="admincp.php?tab=Multi-accounts&aUserID='.$Member->userID.'&bUserIDs='.$userIDs.'" class="light">'.
+				$modActions[] = '<a href="admincp.php?tab=Multi-accoun&aUserID='.$Member->userID.'&bUserIDs='.$userIDs.'" class="light">'.
 					$Member->memberCountryName().'</a>';
 			}
 		}
