6ee9bc295abe36a5504166ea370995fb7624d50c
Kestas Kuliukas <kestas@kuliukas.com>
2011-09-24 18:16:56 +0800

    Multi-accounter tab fix

--- board.php
+++ board.php
@@ -317,7 +317,7 @@ if($User->type['Moderator'])
 			$modActions[] = '<br />Multi-check:';
 			foreach($Game->Members->ByCountryID as $countryID=>$Member)
 			{
-				$modActions[] = '<a href="admincp.php?aUserID='.$Member->userID.'&bUserIDs='.$userIDs.'" class="light">'.
+				$modActions[] = '<a href="admincp.php?tab=Multi-accounts&aUserID='.$Member->userID.'&bUserIDs='.$userIDs.'" class="light">'.
 					$Member->memberCountryName().'</a>';
 			}
 		}
