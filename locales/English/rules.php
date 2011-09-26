4893918aef9ce73346dde008891e964cb2d1fc42
Sleepcap <Oliver.Auth@rhoen.de>
2011-09-26 19:26:17 +0200

    1. Change the name from "rulebook" to etiquette in help.php 2. Use a new setting in config.php "ModEMail", if it does not find it use "adminEMail" for the moderator-email 3. Add more anchors in rules.php: "MultiAccounting", "MetaGaming", "Press", "Mods", "Pause", "Account", "Spam", "Answer" and "CommonSense"
    Signed-off-by: Sleepcap <Oliver.Auth@rhoen.de>

--- locales/English/rules.php
+++ locales/English/rules.php
@@ -58,22 +58,22 @@ font-style:normal;
 <div class="hr"></div>
 
 <a name="Rules"></a><h3><img src="images/icons/alert.png" alt=" " /> Rules:</h3>
-<h4>1. No Multi-Accounting</h4>
+<a name="MultiAccounting"></a><h4>1. No Multi-Accounting</h4>
 <p>
   You may only have <em>one account</em>, second accounts are not allowed under <em>any circumstances</em>, and will be banned. This may also lead to your first account also being banned.&nbsp; If you forget your password, use the lost password finder <a class="light" href="logon.php?forgotPassword=1">here</a>. If you are still unable to log in, contact the mods.
 </p>
 
-<h4>2. No Meta-gaming</h4>
+<a name="MetaGaming"></a><h4>2. No Meta-gaming</h4>
 <p>
   You can't make alliances <em>for reasons outside a game</em>, such as because you are friends, relatives or in return for a favour in another game.&nbsp; This is known as metagaming and is against the rules because it gives an unfair advantage to those involved.&nbsp; If you are worried that you can't stab someone because you want to stay friends, then that's fair enough but you can't join a game with them.
 </p>
 
-<h4>3. Don't work around game messaging/press rules.</h4>
+<a name="Press"></a><h4>3. Don't work around game messaging/press rules.</h4>
 <p>
   If you enter a game with limited press/messaging (such as global-only) then <em>you can't use other methods</em> such as email or PM to bypass this.
 </p>
 
-<h4>4. Do not use the software or moderators as a diplomacy tactic</h4>
+<a name="Mods"></a><h4>4. Do not use the software or moderators as a diplomacy tactic</h4>
 <p>
   Lying is fine in Diplomacy <em>except when it gives mods/admins unnecessary work to do!</em><br /><br />
   Falsely accusing someone of being a multi-accounter/meta-gaming to rally others against them in a game, or claiming that your orders didn't
@@ -83,25 +83,28 @@ font-style:normal;
   <em>must be genuine</em>, even if only told to another player.
 </p>
 
-<h4>5. The Pause/Unpause feature is not a diplomatic tool</h4>
+<a name="Pause"></a><h4>5. The Pause/Unpause feature is not a diplomatic tool</h4>
 <p>
   The pause is there to stop players missing their orders, but is <em>not part of the game</em>.&nbsp; As such pausing or unpausing should not be used for diplomatic gain, such as refusing to unpause unless other players will draw the game.&nbsp; If it is being abused staff may step in to sort it out.
 </p>
 
-<h4>6. You may not access another player's account without permission</h4>
+<a name="Account"></a><h4>6. You may not access another player's account without permission</h4>
 <p>
   In general, account sitting (getting someone you trust to log on as you and issue orders when you are unable to) is encouraged.&nbsp; However, you must not get a sitter or sit for somebody you share games with. Also, anything done by the sitter whilst on your account is your responsibility, so make sure you're happy with that before you give them your password.<br />
   It is also <em>your responsibility</em> to change your password after allowing someone to use your account.
 </p>
-<h4>7. Do not spam the forum or private messaging system</h4>
+
+<h4><a name="Spam"></a>7. Do not spam the forum or private messaging system</h4>
 <p>
   You are welcome and indeed encouraged to join in with the community but be sensible.&nbsp; Try to keep replies relevant to the conversation and don't start too many threads at once. Before posting a question relating to the software please read the <a href="faq.php" class="light">FAQ</a>.
 </p>
-<h4>8. You must answer communication from site staff</h4>
+
+<a name="Answer"></a><h4>8. You must answer communication from site staff</h4>
 <p>
   You must answer any warnings or other messages sent to you by the moderators, because if you don't reply it will be assumed you have something to hide, and your account may be closed.&nbsp; As part of this, you <em>must make sure</em> that the email address provided is one you still check, because this normally how staff will communicate with you.
 </p>
-<h4>9. Follow common sense</h4>
+
+<a name="CommonSense"></a><h4>9. Follow common sense</h4>
 <p>
   The rules above are mainly ones which might not be obvious; just because a specific rule isn't given here doesn't mean
   moderators can't stop people ruining the server for others!
@@ -179,7 +182,8 @@ developer forum</a>).</p>
 <ul>
 <li>Use the game/user reporting tool, present at the bottom of game and user-profile pages, to lodge a report that mods will see. <i>(Currently in beta.)</i></li>
 <li>
-  Email the moderators at <a href="mailto:webdipmod@gmail.com" class="light">webdipmod@gmail.com</a>, <em>not their personal addresses</em>.
+  Email the moderators at <a href="mailto:<?php print (isset(Config::$modEMail) ? Config::$modEMail : Config::$adminEMail); ?>" class="light">
+  <?php print (isset(Config::$modEMail) ? Config::$modEMail : Config::$adminEMail); ?></a>, <em>not their personal addresses</em>.
 </li>
 </ul>
 
