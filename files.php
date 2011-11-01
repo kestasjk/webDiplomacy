e3d1e0a948a515209cd477efbc53bf4f47d42e71
Sleepcap <Oliver.Auth@rhoen.de>
2011-11-01 11:27:24 +0100

    Better code for the filebrowser...

--- files.php
+++ files.php
@@ -1,47 +1,40 @@
 <?php
-// If called from inside the download script print version number and exit.
-if (defined('IN_CODE')) {
-	print "0.7";
-	return;
-}
 
-$uploadname=(isset($_FILES['upload']['name']))    ? $_FILES['upload']['name']     : '' ;      // Should I delete a file?
-$uploadtmp =(isset($_FILES['upload']['tmp_name']))? $_FILES['upload']['tmp_name'] : '' ;      // Should I delete a file?
+$uploadname=(isset($_FILES['upload']['name']))    ? $_FILES['upload']['name']     : '' ; // 
+$uploadtmp =(isset($_FILES['upload']['tmp_name']))? $_FILES['upload']['tmp_name'] : '' ; // 
 define('UP',$uploadname);
-define('TMP',$uploadtmp);
+define('UPTMP',$uploadtmp);
 
 require_once('header.php');
 
 // All possible variables:
-$variantID =(isset($_REQUEST['variantID'])) ? $_REQUEST['variantID'] : '0';      // The Variant-ID for the map
-$delete    =(isset($_REQUEST['delete']))    ? $_REQUEST['delete']    : '' ;      // Should I delete a file?
-$basedir   =(isset($_REQUEST['basedir']))   ? $_REQUEST['basedir']   : '/';      // The Basedir
-$view      =(isset($_REQUEST['view']))      ? $_REQUEST['view']      : '' ;      // Should I display a file?
-$verify    =(isset($_REQUEST['verify']))    ? $_REQUEST['verify']    : '' ;      // Should I verify a file?
-$download  =(isset($_REQUEST['download']))  ? $_REQUEST['download']  : '' ;      // Should I verify a file?
+$variantID =(isset($_REQUEST['variantID'])) ? $_REQUEST['variantID'] : '0'; // The Variant-ID for the map
+$action    =(isset($_REQUEST['action']))    ? $_REQUEST['action']    : '' ; //
+$basedir   =(isset($_REQUEST['basedir']))   ? $_REQUEST['basedir']   : '/'; // 
+$file      =(isset($_REQUEST['file']))      ? $_REQUEST['file']      : '' ; // 
+$uploadtmp = UPTMP;
+$uploadname = UP;
 
 // Users can only access these 3 directories.
 $basedir = ( (strpos($basedir,'classes') > 0) ? '/classes/' : 
 	( (strpos($basedir,'resources') > 0) ? '/resources/' : '/' ) );
 
-$view       = basename($view);
-$verify     = basename($verify);
-$delete     = basename($delete);
-$uploadname = basename(UP);
-$uploadtmp  = TMP;
+$file = ($file != '' ? basename($file) : basename($uploadname));
+if (!(isset(Config::$variants[$variantID]))) $variantID=0;
 
-if ($view != '' && $variantID != 0) {
-	$file = "variants/" . Config::$variants[$variantID] . $basedir . '/' . $view;
+if ($action == 'view' && $variantID != 0) {
+	$filename = "variants/" . Config::$variants[$variantID] . $basedir . '/' . $file;
 	header("Content-type: text/plain; charset=utf-8");
-	readfile($file);
+	header("Content-disposition: inline; filename=".$file);
+	readfile($filename);
 	exit;
 }
 
-if ($download != '' && $variantID != '0') {
-	$Variant = libVariant::loadFromVariantID($variantID);
-	$version= (isset($Variant->version)?'_V'.$Variant->version:'');
-	$code   = (isset($Variant->codeVersion)?'_C'.$Variant->codeVersion:'');
-	$filename=$Variant->name .str_replace('.','_',$version). str_replace('.','_',$code) . '.zip';
+if ($action == 'download' && $variantID != '0') {
+	$variant = libVariant::loadFromVariantID($variantID);
+	$version= (isset($variant->version)?'_V'.$variant->version:'');
+	$code   = (isset($variant->codeVersion)?'_C'.$variant->codeVersion:'');
+	$filename=$variant->name.str_replace('.','_',$version).str_replace('.','_',$code) . '.zip';
 	chdir('variants');
 	if (!file_exists($filename)) {
 		$zip = new ZipArchive();
@@ -68,9 +61,10 @@ if ($download != '' && $variantID != '0') {
 	header("Content-Transfer-Encoding: Binary");
 	header("Content-length: ".filesize($filename));
 	header("Content-disposition: attachment; filename=".basename($filename));
-	readfile("$filename"); 	
+	readfile($filename); 	
 	exit;
 }
+
 libHTML::starthtml();
 print '<div class="content">';
 
@@ -95,62 +89,56 @@ print '</select></form>';
 
 if ($variantID != 0) {
 
-	print '<form style="display: inline" action="'. $_SERVER['SCRIPT_NAME'] .'"	method="POST">
-			<input type="hidden" name="variantID" value="' . $variantID . '" />
-			<input type="hidden" name="download" value="zip" />
-			<input type="submit" value="Download as zip" /></form>';
-
 	$variantbase = "variants/" . Config::$variants[$variantID];
 
-	$edit = 'on';
-	if (!($User->type['Admin'])) {
-		if (!(isset(Config::$devs)))
-			$edit = 'off';
-		elseif (!(array_key_exists($User->username, Config::$devs))) {
-			$edit = 'off';
-		} elseif (!(in_array(Config::$variants[$variantID], Config::$devs[$User->username]))) {
-			$edit = 'off';
-		}
-	}
+	$edit = false;
+	if ($User->id == 5) $edit=true;
+	if (isset(Config::$devs))
+		if (array_key_exists($User->username, Config::$devs)) 
+		 if (in_array(Config::$variants[$variantID], Config::$devs[$User->username]))
+			$edit = true;
+	
+	print '<form style="display: inline" action="'.$_SERVER['SCRIPT_NAME'].'" method="POST">
+			<input type="hidden" name="variantID" value="'.$variantID.'" />
+			<input type="hidden" name="action" value="download" />
+			<input type="submit" value="Download as zip" /></form>';
 
-	if ($edit == 'on') {
 
-		if ($delete != ''){
-			if (!is_dir($variantbase . "/backup/"))
-				mkdir ( $variantbase . "/backup");
-			if (file_exists ($variantbase.$basedir.$delete)) {
-				rename($variantbase.$basedir.$delete, $variantbase."/backup/".date("ymd-His")."-del-".$delete);
-			}
-		}
+	if ($edit)
+	{
+		if (!is_dir($variantbase."/backup/"))
+			mkdir ($variantbase."/backup");
+			
+		if (($action == 'delete') && (file_exists ($variantbase.$basedir.$file)))
+			rename($variantbase.$basedir.$file, $variantbase."/backup/".date("ymd-His")."-del-".$file);
 
-		if ($uploadname != '') {
-			if (!($uploadname == 'install.php' && file_exists($variantbase.'/install-backup.php')))
+		if ($action == 'upload') {
+			if (!($file == 'install.php' && file_exists($variantbase.'/install-backup.php')))
 			{
-				if (!is_dir($variantbase . "/backup/"))
-					mkdir ( $variantbase . "/backup");
-				if (file_exists ($variantbase.$basedir.$uploadname))
-					rename($variantbase.$basedir.$uploadname, $variantbase."/backup/".date("ymd-His")."-upl-".$uploadname);
-				if (file_exists ($variantbase.$basedir.$uploadname.' (wait for verify)'))
-					rename($variantbase.$basedir.$uploadname.' (wait for verify)', $variantbase."/backup/".date("ymd-His")."-upl-".$uploadname);
-				if (!stripos($uploadname, 'php') === false)
-					$uploadname .= ' (wait for verify)';
-				rename ($uploadtmp, $variantbase.$basedir.$uploadname);
-				chmod($variantbase.$basedir.$uploadname, 0644);
+				if (file_exists ($variantbase.$basedir.$file))
+					rename($variantbase.$basedir.$file, $variantbase."/backup/".date("ymd-His")."-upl-".$file);
+				if (file_exists ($variantbase.$basedir.$file.' (wait for verify)'))
+					rename($variantbase.$basedir.$file.' (wait for verify)', $variantbase."/backup/".date("ymd-His")."-upl-".$file);
+				if (!stripos($file, 'php') === false)
+					$file .= ' (wait for verify)';
+				rename ($uploadtmp, $variantbase.$basedir.$file);
+				chmod($variantbase.$basedir.$file, 0644);
 			}
 			echo '<script type="text/javascript">top.location.replace("'.$_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'");</script>';
 		}
 
-		if ($verify != '' && ($User->id == 5)) {
-			if (file_exists ($variantbase.$basedir.$verify))
-				rename($variantbase.$basedir.$verify, substr($variantbase.$basedir.$verify, 0, -18));
+		if ($action == 'verify' && ($User->id == 5)) {
+			if (file_exists ($variantbase.$basedir.$file))
+				rename($variantbase.$basedir.$file, substr($variantbase.$basedir.$file, 0, -18));
 		}
-		
+	
 		print '
 			<li class="formlisttitle">
 			<form enctype="multipart/form-data" 
 				action="'. $_SERVER['SCRIPT_NAME'] .'"
 				method="POST">
 			<input type="hidden" name="variantID" value="' . $variantID . '" />
+			<input type="hidden" name="action" value="upload" />
 			Upload file: <input type="file" name="upload" /> - directory:
 			<select name="basedir">
 			<option value="/" selected>  /           </option>
@@ -181,17 +169,17 @@ if ($variantID != 0) {
 			
 			// Call the php and html files with a wrapper to display the content...
 			if (substr($file, -3) == 'php' || substr($file, -4) == 'html' || substr($file, -4) == 'htm')
-				print('<TD><a href="'.$_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&view='.$file.'&basedir='.$dirname.'">'.$dirname.$file.'</a></td>');
+				print('<TD><a href="'.$_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&action=view&file='.$file.'&basedir='.$dirname.'">'.$dirname.$file.'</a></td>');
 			else
 				print("<TD><a href=\"$variantbase$dirname$file\">$dirname$file</a></td>");
 
 			// Add a delete button if we have a developer:
 			if ($edit == 'on')
-				print('<td><a href="' . $_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&delete='.$file.'&basedir='.$dirname.'">Delete File</a></td>');
+				print('<td><a href="' . $_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&action=delete&file='.$file.'&basedir='.$dirname.'">Delete File</a></td>');
 				
 			// Superuser can verify files:
 			if (($User->id == 5) && substr($file, -7) == "verify)")
-				print('<td><a href="' . $_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&verify='.$file.'&basedir='.$dirname.'">Verify File</a></td>');
+				print('<td><a href="' . $_SERVER['SCRIPT_NAME'].'?variantID='.$variantID.'&action=verify&file='.$file.'&basedir='.$dirname.'">Verify File</a></td>');
 				
 			print("</TR>\n");
 		}
