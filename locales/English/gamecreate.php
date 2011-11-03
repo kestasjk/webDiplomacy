8c0f578eb474fee0116efa32973687d95a3173be
Sleepcap <Oliver.Auth@rhoen.de>
2011-11-03 21:17:20 +0100

    Minbet now back to 3 DPoints It's needed so a pot of a new created no 2-player game has 3-dpoints at least and will get caught by the new RR-filter

--- locales/English/gamecreate.php
+++ locales/English/gamecreate.php
@@ -74,7 +74,7 @@ Start a new game; you decide the name, how long it runs, and how much it's worth
 	</li>
 
 	<li class="formlisttitle">
-		Bet size: (1<?php print libHTML::points(); ?>-
+		Bet size: (3<?php print libHTML::points(); ?> -
 			<?php print $User->points.libHTML::points(); ?>)
 	</li>
 	<li class="formlistfield">
