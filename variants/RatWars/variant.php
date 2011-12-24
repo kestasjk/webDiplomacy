0e503c4da7270a7f3e0dcbe962beec08f13293ae
Sleepcap <Oliver.Auth@rhoen.de>
2011-12-24 07:17:15 +0100

    RatWars: copyright header added / Overlay changes.

--- variants/RatWars/variant.php
+++ variants/RatWars/variant.php
@@ -1,4 +1,27 @@
 <?php
+/*
+	Copyright (C) 2011 kaner406 / Oliver Auth
+
+	This file is part of the Rat Wars variant for webDiplomacy
+
+	The Rat Wars variant for webDiplomacy is free software: you can
+	redistribute it and/or modify it under the terms of the GNU Affero General
+	Public License as published by the Free Software Foundation, either version
+	3 of the License, or (at your option) any later version.
+
+	The Rat Wars variant for webDiplomacy is distributed in the hope
+	that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with webDiplomacy. If not, see <http://www.gnu.org/licenses/>.
+
+	---
+	
+	Changelog:
+	1.0:     initial release
+*/
 
 defined('IN_CODE') or die('This script can not be run by itself.');
 
@@ -11,7 +34,7 @@ class RatWarsVariant extends WDVariant {
 	public $author     ='kaner406';
 	public $adapter    ='kaner406 / Oliver Auth';
 	public $version    ='1';
-	public $codeVersion='1.0.1';	
+	public $codeVersion='1.0.2';	
 	
 	public $countries=array('Dead Rabbits','Plug Uglies','Shirt Tails','Hell-Cats');	
 
