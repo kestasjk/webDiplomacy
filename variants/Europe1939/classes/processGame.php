8f0901de7eb026bee07eecc0b041e0e5564a1d81
Sleepcap <Oliver.Auth@rhoen.de>
2012-02-28 21:46:08 +0100

    New variant: Europe 1939

---
+++ variants/Europe1939/classes/processGame.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	Copyright (C) 2012 Mikalis Kamaritis / Oliver Auth
+
+	This file is part of the Europe 1939 variant for webDiplomacy
+
+	The Europe 1939 variant for webDiplomacy is free software: you can
+	redistribute it and/or modify it under the terms of the GNU Affero General
+	Public License as published by the Free Software Foundation, either version
+	3 of the License, or (at your option) any later version.
+
+	The Europe 1939 variant for webDiplomacy is distributed in the hope
+	that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with webDiplomacy. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+defined('IN_CODE') or die('This script can not be run by itself.');
+
+class NeutralUnits_processGame extends processGame
+{
+	function process()
+	{
+		global $DB;
+		parent::process();
+		
+		// If only the "neutral player has to do retreats process again.
+		if ($this->phase == 'Retreats')
+		{	
+			list($count) = $DB->sql_row("SELECT COUNT(*)
+				FROM wD_Members 
+				WHERE orderStatus != 'None' AND gameID = ".$this->id);
+			if ($count == 0)
+				parent::process();
+		}	
+	}
+}
+
+class Europe1939Variant_processGame extends NeutralUnits_processGame {}
