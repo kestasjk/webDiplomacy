20dea24345ca75d38e2bd00fa15f646e148f5b7f
Sleepcap <Oliver.Auth@rhoen.de>
2012-03-27 23:46:04 +0200

    New reliability-rating explanation

--- locales/English/reliability.php
+++ locales/English/reliability.php
@@ -9,41 +9,44 @@ defined('IN_CODE') or die('This script can not be run by itself.');
 ?>
 
 <p class="intro">
-Because Diplomacy is a game of communication, trust (and distrust), and because games usually take a long time to finish
-it's very important for players that you play the best you can and don't screw the game halfway.
-</p>
+
+Because Diplomacy is a game of communication, trust (and distrust), 
+and because games usually take a long time to finish it's very important for 
+players that you play the best you can and don't screw the game halfway.</p>
 
 <p class="intro">
-The reliable rating is an easy calculation that represents how reliable you enter your commands and how reliable you play
-your games till the end.
+
+The reliable rating is an easy calculation that represents how reliable 
+you enter your commands and how reliable you play your games till the end.
 </p>
 
 <div class="hr" ></div>
 
 <p class="intro">
-Your rating is dependend on 2 important factors. How many phases you missed to enter orders in comparison to your total phases played, 
-and how many games your country went into CivilDisorder, because you didn't even check the gamepage for 2 turns in a row.<br>
-The first part is 100 minus phases missed / phases played * 200, not to be lower than 0.<br>
+Your rating is dependent on 2 important factors. How many phases you missed 
+to enter orders in comparison to your total phases played, and how many games 
+you left before the end, because you didn't check the game page for 2 turns in a row.
 <b>Example</b>: If a user misses 5% of their games, rating would be 90, 15% would be 70, etc<br>
-From this rating we substract 10% for each game you left bevore the end.
+From this rating we subtract 10% for each game you left before the end.
 </p>
 
 <p class="intro">
-The penality for the "Left" games seems a bit harsh, but many games get totally screwed if a player does not play the game till the end.
-Most of the time some countries gain really big unearned advantages. But you can even out your lost reliability by taking an open spot 
-from a game another player left.
+The penalty for the "Left" games seems a bit harsh, but many games get totally 
+screwed if a player does not play the game till the end. Most of the time some 
+countries gain really big unearned advantages. But you can even out your lost 
+reliability by taking an open spot from a game another player left.
 </p>
 
 <p class="intro">
-When someone creates a game they can select a minimum rating for the people able to enter their games, and if you rating is too 
-low you might not be able to join all the games as you like.<br>
-Also for each 10% of reliability you can join 1 game. If your reliability is 90% or better you can join as many games as you want.
-</p>
+When someone creates a game they can select a minimum rating for the people able to enter their games, 
+and if you rating is too low you might not be able to join all the games as you like.<br>
+Also for each 10% of reliability you can join 1 game. If your reliability is 90% or better you can join as many games as you want.</p>
 
 <div class="hr" ></div>
 
 <p class="intro">
 <b>Why should I continue a game if my country can't win?</b><br>
-If you can't win a game or are on a loosing spot you might choose to hurt the country that sealed your failure as much as possible by making
-your defeat as hard as possible. Talk to stronger players on the board, thay might help you, just because you have a common enemy.
+If you can't win a game or are on a losing position you might choose to hurt the country that sealed 
+your failure as much as possible by making your defeat as hard as possible. Talk to stronger players 
+on the board, they might help you, just because you have a common enemy.
 </p>
