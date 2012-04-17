0045450f43a32e6951ca5e74eedb814aa442dce0
Oliver Auth <Oliver.Auth@rhoen.de>
2012-04-17 23:51:56 +0200

    Errorfix countryswitch

--- locales/English/countryswitch.php
+++ locales/English/countryswitch.php
@@ -72,31 +72,33 @@ if ( isset($_REQUEST['newSwitch']) )
 	$fromID = (int)$User->id;
 	$toID   = (int)$form['toID'];
 	$gameID = (int)$form['gameID'];
-	
-	$Variant=libVariant::loadFromGameID($gameID);
-	$Game = $Variant->Game($gameID);
-	$SendUser = new User($toID);
-	
-	// Check if there is a mute against a player
-	list($muted) = $DB->sql_row("SELECT count(*) FROM wD_Members AS m
-								LEFT JOIN wD_MuteUser AS f ON ( m.userID = f.userID )
-								LEFT JOIN wD_MuteUser AS t ON ( m.userID = t.muteUserID )
-							WHERE m.gameID = ".$Game->id." AND (f.muteUserID =".$SendUser->id." OR t.userID =".$SendUser->id.")");
-							
-	// Check for additional requirements:
-	if ( $Game->minPhases > $SendUser->phasesPlayed)
-		print '<b>ERROR.</b>The User you selected did not play enough phases to join this game.<hr>';
-	elseif ( $Game->minRating > $SendUser->getReliability())
-		print '<b>ERROR.</b>The reliability of User you selected is not high enough to join this game.<hr>';
-	elseif ( count($Variant->countries)>2 && $message = $SendUser->isReliable())
-		print '<b>ERROR.</b>The User you selected can not join new games at the moment.<hr>';
-	elseif ( array_key_exists ( $toID , $Game->Members->ByUserID))
-		print '<b>ERROR.</b>The User you selected is already a member of this game.<hr>';
-	elseif ( $muted > 0)
-		print "<b>ERROR.</b>The User you selected can't join. A player in this game has him muted or he muted a player in this game<hr>";
-	else
-		$DB->sql_put('INSERT INTO wD_CountrySwitch (fromID, toID, gameID, status) VALUES ('.
-			$fromID.','.$toID.','.$gameID.', "Send")');
+	if ( $toID > 0)
+	{
+		$Variant=libVariant::loadFromGameID($gameID);
+		$Game = $Variant->Game($gameID);
+		$SendUser = new User($toID);
+		
+		// Check if there is a mute against a player
+		list($muted) = $DB->sql_row("SELECT count(*) FROM wD_Members AS m
+									LEFT JOIN wD_MuteUser AS f ON ( m.userID = f.userID )
+									LEFT JOIN wD_MuteUser AS t ON ( m.userID = t.muteUserID )
+								WHERE m.gameID = ".$Game->id." AND (f.muteUserID =".$SendUser->id." OR t.userID =".$SendUser->id.")");
+								
+		// Check for additional requirements:
+		if ( $Game->minPhases > $SendUser->phasesPlayed)
+			print '<b>ERROR.</b>The User you selected did not play enough phases to join this game.<hr>';
+		elseif ( $Game->minRating > $SendUser->getReliability())
+			print '<b>ERROR.</b>The reliability of User you selected is not high enough to join this game.<hr>';
+		elseif ( count($Variant->countries)>2 && $message = $SendUser->isReliable())
+			print '<b>ERROR.</b>The User you selected can not join new games at the moment.<hr>';
+		elseif ( array_key_exists ( $toID , $Game->Members->ByUserID))
+			print '<b>ERROR.</b>The User you selected is already a member of this game.<hr>';
+		elseif ( $muted > 0)
+			print "<b>ERROR.</b>The User you selected can't join. A player in this game has him muted or he muted a player in this game<hr>";
+		else
+			$DB->sql_put('INSERT INTO wD_CountrySwitch (fromID, toID, gameID, status) VALUES ('.
+				$fromID.','.$toID.','.$gameID.', "Send")');
+	}
 }
 
 ?>
