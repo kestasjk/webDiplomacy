6f4832d0a15821031fc1a9be05c02f8af52987c9
Oliver Auth <Oliver.Auth@rhoen.de>
2012-10-29 11:16:27 +0100

    New variant: ClassicCatalysm

---
+++ variants/ClassicCataclysm/classes/OrderInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('IN_CODE') or die('This script can not be run by itself.');
+
+// Build anywhere:
+class ClassicCataclysmVariant_OrderInterface extends OrderInterface
+{
+	protected function jsLoadBoard()
+	{
+		global $Variant;
+		parent::jsLoadBoard();
+		
+		$seaTerrs='Array("'.implode($Variant->seaTerrs, '","').'")';
+
+		libHTML::$footerIncludes[] = '../variants/ClassicCataclysm/resources/units_and_icons_correct.js';
+		foreach(libHTML::$footerScript as $index=>$script)
+			if(strpos($script, 'loadOrdersPhase();') )
+				libHTML::$footerScript[$index]=str_replace('loadOrdersPhase();','loadOrdersPhase(); NewUnitNames('.$seaTerrs.');', $script);			
+		foreach(libHTML::$footerScript as $index=>$script)
+			if(strpos($script, 'loadOrdersModel();') )
+				libHTML::$footerScript[$index]=str_replace('loadOrdersModel();','loadOrdersModel();IconsCorrect('.$seaTerrs.');', $script);
+	}
+}
---
+++ variants/ClassicCataclysm/resources/units_and_icons_correct.js
@@ -0,0 +1,72 @@
+Array.prototype.inArray = function (value)	{
+	var i;
+	for (i=0; i < this.length; i++) {
+		if (this[i] == value) {
+			return true;
+		}
+	}
+	return false;
+};	
+
+function IconsCorrect(SeaTerrs)
+{
+	MyOrders.map(function(OrderObj) {
+			OrderObj.setUnitIconArea = function(newIcon) {
+				if (SeaTerrs.inArray(this.Unit.Territory.name))
+					newIcon='fleet';
+					
+				if( newIcon == this.currentUnitIcon ) return;
+		
+				if( this.currentUnitIcon != false )
+					$('orderID'+this.id).removeClassName(this.currentUnitIcon.toLowerCase());
+		
+				$('orderID'+this.id).addClassName(newIcon.toLowerCase());
+				this.currentUnitIcon=newIcon;
+		
+				this.unitIconArea.update('<img src="images/'+newIcon+'.png" alt="'+newIcon+'" />');
+			};
+		});
+}
+
+function NewUnitNames(SeaTerrs)
+{
+	if( context.phase == 'Builds' ) {
+
+		MyOrders.map(function(OrderObj) {
+			OrderObj.updateTypeChoices = function () {
+				switch(this.type)
+				{
+					case 'Build Army':
+					case 'Build Fleet':
+					case 'Wait':
+						this.typeChoices = $H({'Build Army':'Build a unit',
+									'Wait':'Wait/Postpone build'});
+						break;
+					case 'Destroy':
+						this.typeChoices = $H({'Destroy':'Destroy a unit'});
+				}
+				
+				return this.typeChoices;
+			};
+			OrderObj.updateTypeChoices();	
+			OrderObj.reHTML('type');
+		}, this);
+		
+	} else {	
+
+		MyOrders.map(function(OrderObj) {
+			OrderObj.beginHTML = function () {
+				if (SeaTerrs.inArray(this.Unit.Territory.name))
+					return 'The fleet at '+this.Unit.Territory.name+' ';
+				else
+					return 'The army at '+this.Unit.Territory.name+' ';			
+			};
+			OrderObj.reHTML('orderBegin');			
+			OrderObj.setSelectsGreen();
+		}, this);
+	}
+	
+}			
+
+
+
