bcade61ef57073323a036adc217ce3f3aaed8e7d
Oliver Auth <Oliver.Auth@rhoen.de>
2012-11-20 16:33:47 +0100

    Reliability-text updated...

--- locales/English/reliability.php
+++ locales/English/reliability.php
@@ -25,22 +25,22 @@ you enter your commands and how reliable you play your games till the end.
 <p class="intro">
 Your rating is dependent on 2 important factors. How many phases you missed 
 to enter orders in comparison to your total phases played, and how many games 
-you left before the end, because you didn't check the game page for 2 turns in a row.
-<b>Example</b>: If a user misses 5% of their games, rating would be 90, 15% would be 70, etc<br>
-From this rating we subtract 10% for each game you left before the end.
+you left before the end, because you didn't check the game page for 2 turns in a row.<br>
+<b>Example</b>: If a user misses 5% of their games, rating would be 90, 15% would be 70, etc.
 </p>
 
 <p class="intro">
+From this rating we subtract 10% for each game you left before the end.
 The penalty for the "Left" games seems a bit harsh, but many games get totally 
 screwed if a player does not play the game till the end. Most of the time some 
-countries gain really big unearned advantages. But you can even out your lost 
-reliability by taking an open spot from a game another player left.
+countries gain really big unearned advantages.
+<br>But you can even out your lost reliability by taking <b>an open spot from a game</b> another player left.
 </p>
 
 <p class="intro">
 When someone creates a game they can select a minimum rating for the people able to enter their games, 
 and if you rating is too low you might not be able to join all the games as you like.<br>
-Also for each 10% of reliability you can join 1 game. If your reliability is 90% or better you can join as many games as you want.</p>
+Also for each 10% of reliability you can join 1 game. If your reliability is <b>91% or better</b> you can join as many games as you want.</p>
 
 <div class="hr" ></div>
 <p class="intro">
@@ -49,9 +49,7 @@ The current grades are:<br>
 <?php
 	require_once ("lib/reliability.php");
 	foreach (libReliability::$grades as $limit=>$grade)
-	{
 		print $grade." = ".$limit." or better.<br>";
-	}
 ?>
 This grading-system might change in the future.
 <div class="hr" ></div>
