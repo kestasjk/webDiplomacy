e98f2a61fe72eb4659debce09d2b1c1a6caaae0a
Oliver Auth <Oliver.Auth@rhoen.de>
2013-01-09 23:16:12 +0100

    Modforum pager works now.

--- modforum.php
+++ modforum.php
@@ -4,9 +4,34 @@
  * @package Base
  */
 require_once('header.php');
-require_once('pager/pagerthread.php');
+require_once('pager/pager.php');
 
 $User->clearNotification('ModForum');
+
+class PagerThread extends Pager
+{
+	public static $defaultPostsPerPage=30;
+	public $type='thread';
+	function __construct($itemsTotal, $threadID)
+	{
+		parent::__construct('modforum.php',$itemsTotal,self::$defaultPostsPerPage);
+		$this->addArgs('threadID='.$threadID);
+	}
+	function getCurrentPage($currentPage=1)
+	{
+		parent::getCurrentPage($this->pageCount);
+		if ( $this->currentPage>$this->pageCount )
+			$this->currentPage = $this->pageCount;
+	}
+	function currentPageNumber()
+	{
+		return parent::currentPageNumber();
+		if( $this->currentPage != $this->pageCount )
+			return parent::currentPageNumber();
+		else
+			return '';
+	}
+}
 
 class PagerForum extends Pager
 {
