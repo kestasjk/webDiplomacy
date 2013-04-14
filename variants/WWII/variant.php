4c3c085111d88d7bc6db3bda2081364c177294ea
Oliver Auth <Oliver.Auth@rhoen.de>
2013-04-14 22:57:40 +0200

    WWII: fixed a map-problem on the smallmap

--- variants/WWII/variant.php
+++ variants/WWII/variant.php
@@ -10,7 +10,7 @@ class WWIIVariant extends WDVariant {
 	public $description= 'World War II variant by Synapse';
 	public $adapter    = 'Synapse';
 	public $version    = '1';
-	public $codeVersion= '1.0';
+	public $codeVersion= '1.0.1';
 
 	public $countries=array('France','Germany', 'Britain', 'Italy', 'Soviet Russia');
 
