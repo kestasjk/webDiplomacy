bc86d5fd723ed27d1684e9b051805d3518387d0b
Kestas Kuliukas <kestas@kuliukas.com>
2013-06-01 01:36:24 +0800

    Board chat archive pagination

--- board/info/messages.php
+++ board/info/messages.php
@@ -27,12 +27,21 @@ defined('IN_CODE') or die('This script can not be run by itself.');
  */
 
 
+require_once('pager/pagerarchive.php');
+
+list($itemsTotal) = $DB->sql_row("SELECT COUNT(*) FROM wD_GameMessages WHERE gameID = ".$Game->id." AND ".
+	"toCountryID = 0".(isset($Member)?" OR fromCountryID = ".$Member->countryID." OR toCountryID = ".$Member->countryID:''));
+$pager = new PagerArchive($itemsTotal, $Game->id, 'Messages');
+
+print $pager->html();
+
+
 print '<h4>'.l_t('Chat archive').'</h4>';
 
 print '<div class="variant'.$Game->Variant->name.'">';
 
 $CB = $Game->Variant->Chatbox();
-print '<table>'.$CB->getMessages( -1, false).'</table>';
+print '<table>'.$CB->getMessages( -1, $pager->SQLLimit()).'</table>';
 
 print '</div>';
 
