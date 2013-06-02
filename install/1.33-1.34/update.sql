d97e940ccfae3c664b3094b09d32ce4ba744f990
Kestas Kuliukas <kestas@kuliukas.com>
2013-06-02 11:36:56 +0800

    Add version update SQL

--- install/1.33-1.34/update.sql
+++ install/1.33-1.34/update.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `wD_Backup_Games` CHANGE `missingPlayerPolicy` `missingPlayerPolicy` ENUM( 'Normal', 'Strict', 'Wait' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'Normal';
 ALTER TABLE `wD_Games` CHANGE `missingPlayerPolicy` `missingPlayerPolicy` ENUM( 'Normal', 'Strict', 'Wait' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'Normal';
+
+UPDATE `wD_Misc` SET `value` = '134' WHERE `name` = 'Version';
