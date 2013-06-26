a71ebc9cb5361f14cfe50c2e9610132019ef2163
Kestas Kuliukas <kestas@kuliukas.com>
2013-06-26 21:10:42 +0800

    Fix for alternative locales in variantData.php

--- variants/variantData.php
+++ variants/variantData.php
@@ -1,4 +1,25 @@
 <?php
+/*
+    Copyright (C) 2004-2013 Kestas J. Kuliukas
+
+	This file is part of webDiplomacy.
+
+    webDiplomacy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    webDiplomacy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with webDiplomacy.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+defined('IN_CODE') or die('This script can not be run by itself.');
+
 
 class VariantData
 {
@@ -53,7 +74,7 @@ class VariantData
 	{
 		global $DB;
 		
-		$DB->sql_put("UPDATE wD_VariantData SET val_".$col." = ".$val." WHERE ".$this->where($offset));
+		$DB->sql_put("UPDATE wD_VariantData SET val_".$col." = ".number_format(round($val,3),3)." WHERE ".$this->where($offset));
 	}
 
 	public function setFloat($val, $offset=0)
