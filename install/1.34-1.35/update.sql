a391ea32673a73228a38f8c3584c8c7a9b15690b
jimbursch <jim@mymindshare.com>
2014-08-28 10:58:57 -0700

    missing semicolon
    Added missing semicolon between sql statements that was causing script
    to fail.

--- install/1.34-1.35/update.sql
+++ install/1.34-1.35/update.sql
@@ -32,7 +32,7 @@ FROM wD_Users u
 WHERE NOT ChanceRussia = 0.142857
 UNION SELECT 1, 948379409, u.id, 7, ChanceTurkey
 FROM wD_Users u
-WHERE NOT ChanceTurkey = 0.142857
+WHERE NOT ChanceTurkey = 0.142857;
 
 
 UPDATE `wD_Misc` SET `value` = '135' WHERE `name` = 'Version';
