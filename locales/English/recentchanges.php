220e2a607924f01ee58ae986290b073925b190db
Timothy Jones <timothy.l.jones@gmail.com>
2015-01-06 09:24:40 +1100

    Added RR and CD takeover changes to recentchanges

--- locales/English/recentchanges.php
+++ locales/English/recentchanges.php
@@ -29,6 +29,12 @@ font-style:normal;
 	margin-left:30px;
 }
 </style>
+<ul>
+<li>Reliability ratings are now live: CDs forced by mods as part of seating players in the right places for tournaments should no longer be counted. RR is calculated as a combination of CD and NMR ratios. The best way to improve your rating is to play more games and don't CD or NMR. CDs from cancelled games are included. So, players no longer need to draw games to ensure that an early CD is recorded.
+</li>
+<li>All CD takeovers now cost half the points that the position is worth.</li>
+<div class="hr"></div>
+</ul>
 
 January 4th, 2015:
 <ul>
