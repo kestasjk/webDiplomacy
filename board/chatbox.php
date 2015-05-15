157c2c20a002be33ceedbb3e93b786aafe70e654
Timothy Jones <timothy.l.jones@gmail.com>
2015-05-15 16:03:11 +1000

    Corrected condition to display country names

--- board/chatbox.php
+++ board/chatbox.php
@@ -422,7 +422,7 @@ class Chatbox
 				$message['message'] = '<span class="messageFromMe">'.$message['message'].'</span>';
 
 			// Display the country name in front of the text (for colorblind people)
-			if ( $User->options->value['colourblind'] != 'Yes')
+			if ( $User->options->value['colourblind'] != 'No')
 			{
 				if(isset($Member) && $Member->countryID == $message['fromCountryID'])
 					$messagestxt .=  '<strong>You:</strong> ';
