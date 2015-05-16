1f5a644fc68b2c3ee1c49487de639257a8759286
Timothy Jones <timothy.l.jones@gmail.com>
2015-05-16 19:22:34 +1000

    Matched update script with master branch

--- install/1.37-1.38/update.sql
+++ install/1.37-1.38/update.sql
@@ -1,9 +1,11 @@
-CREATE TABLE `wD_WatchedGames` (
-		  `userID` mediumint(8) unsigned NOT NULL,		
-		  `gameID` mediumint(8) unsigned NOT NULL,		
-		  KEY `gid` (`gameID`),		
-		  KEY `uid` (`userID`)		
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;		
-		
-		
--UPDATE `wD_Misc` SET `value` = '138' WHERE `name` = 'Version';
+ CREATE TABLE `wD_WatchedGames` (
+	  `userID` mediumint(8) unsigned NOT NULL,
+	  `gameID` mediumint(8) unsigned NOT NULL,
+	  KEY `gid` (`gameID`),
+	  KEY `uid` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE wD_Games ADD `minimumReliabilityRating` tinyint(3) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE wD_Backup_Games ADD `minimumReliabilityRating` tinyint(3) unsigned NOT NULL DEFAULT '0';
+
+UPDATE `wD_Misc` SET `value` = '138' WHERE `name` = 'Version';
