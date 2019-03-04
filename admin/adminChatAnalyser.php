ea89e3401db3b47ef24f27d93f9997daf107c531
jmo1121109 <webdipgit@gmail.com>
2019-03-03 20:11:11 -0500

    Additional AdminCP Updates
    1. Fix a bug I introduced to the multi tool that showed all games as active in the results list.
    
    2. restyle the press pull tool and remove un-needed variables and code blocks.
    
    3. change the accesslogs page to show most recently joined users on top vs bottom and increase max pull size to 1,000 days since performance was faster then expected even on webdip.

--- admin/adminChatAnalyser.php
+++ admin/adminChatAnalyser.php
@@ -1,7 +1,7 @@
 <?php
 
 $countryID1 = $countryID2 = array();
-$gameID=$userID=0;
+$gameID=0;
 $reason = 'Press was checked for gameID: ';
 
 if (!defined('IN_CODE'))
@@ -18,34 +18,25 @@ if (!$User->type['Moderator'])
 $asTxt = (isset($_REQUEST['TXT'])?true:false);
 $asCsv = (isset($_REQUEST['CSV'])?true:false);
 
-if ( isset($_REQUEST['gameID']) )
-{	
+if ( isset($_REQUEST['gameID']) ) {	
     $gameID=(int)$_REQUEST['gameID'];
 }	
 
-if ( isset($_REQUEST['userID']) && $gameID == 0 )
-{	
-    $userID=(int)$_REQUEST['userID'];
-}	
-
-if ( isset($_REQUEST['countryID1']) )
-{
+if ( isset($_REQUEST['countryID1']) ) {
     foreach ($_REQUEST['countryID1'] as $user)
     {
         $countryID1[]=(int)$user;
     }
 }
 
-if ( isset($_REQUEST['countryID2']) )
-{	
+if ( isset($_REQUEST['countryID2']) ) {	
     foreach ($_REQUEST['countryID2'] as $user)
     {	
         $countryID2[]=(int)$user;
     }
 }
 
-if ( isset($_REQUEST['reason']) && $gameID == 0 )
-{	
+if ( isset($_REQUEST['reason']) && $gameID == 0 ) {	
     $reason = htmlentities( $_REQUEST['reason'], ENT_NOQUOTES, 'UTF-8');
 }
 
@@ -66,33 +57,33 @@ if ((($gameID != 0) && (count($countryID1) > 0) && (count($countryID2) > 0)) && 
 	
 	$tabl = $DB->sql_tabl($sql);
 	
-	if ($asCsv)
-	{
+	if ($asCsv) {
 		header("Content-type: csv/plain; charset=utf-8");
 		header("Content-disposition: inline; filename=Chatlog.csv");
 		print '"timeSent","gameDate","To","From","Message"'."\n";
     }
     
-	else
-	{
+	else {
 		header("Content-type: txt/plain; charset=utf-8");
 		header("Content-disposition: inline; filename=Chatlog.txt");
 	}
 		
 	while ( list($message, $to, $from, $turn, $timeSent) = $DB->tabl_row($tabl) )
 	{
-		if ($asCsv)
+		if ($asCsv) {
 			print '"'.gmstrftime("%d %b %y %I:%M %p", $timeSent).'",'.
 					'"'.$Variant->turnAsDate($turn).'",'.
 					'"'.($to==0?'Global':$Variant->countries[$to-1]).'",'.
 					'"'.($from==0?'Global':$Variant->countries[$from-1]).'",'.
-					'"'.addslashes($message).'"'."\n";		
-		else
+					'"'.addslashes($message).'"'."\n";	
+		}	
+		else {
 			print gmstrftime("%d %b %y %I:%M %p", $timeSent).
 					' ('.$Variant->turnAsDate($turn).')'.
 					' / To '.($to==0?'Global':$Variant->countries[$to-1]).
 					' from '.($from==0?'Global':$Variant->countries[$from-1]).': '.
-					$message."\r\n";		
+					$message."\r\n";
+		}		
 	}
 	exit(0);
 }
@@ -101,7 +92,7 @@ if ((($gameID != 0) && (count($countryID1) > 0) && (count($countryID2) > 0)) && 
  * Print a form for selecting which game to check, and which users to check against
  */
 print '<FORM method="get" action="admincp.php">';
-print '<P><STRONG>Game ID: </STRONG><INPUT type="text" name="gameID" value="'.($gameID!=0?$gameID:'').'" length="10" /> <br> ';
+print '<P class = "modTools"><STRONG>Game ID: </STRONG><INPUT class = "modTools" type="text" name="gameID" value="'.($gameID!=0?$gameID:'').'" length="10" /> <br> ';
 
 if ($gameID != 0)
 {
@@ -114,17 +105,14 @@ if ($gameID != 0)
     // Because moderators cannot break anon for their own games do not load the page at all if the mod is in the game which is unfinished. Die and log attempted access.
 	list($modInGame) = $DB->sql_row("SELECT count(1) FROM wD_Members m INNER JOIN wD_Games g ON ( g.id = m.gameID ) WHERE m.userID = ".$User->id." and m.gameID =".$gameID." AND NOT g.phase = 'Finished'");
 	
-    if ($modInGame == 1)
-    {
+    if ($modInGame == 1) {
         $reason = "Press was not checked on gameID:". $gameID. " because the moderator was in the game.";
         $DB->sql_put("INSERT INTO wD_AdminLog ( name, userID, time, details, params )
                     VALUES ( 'CheckGamePress', ".$User->id.", ".time().", '".$reason."', '' )");
 
         die ('Do not check press for games you are in! This has been logged.');
     }
-    
-    else
-    {
+    else {
         $reason = $reason . $gameID;
         $DB->sql_put("INSERT INTO wD_AdminLog ( name, userID, time, details, params )
             VALUES ( 'CheckGamePress', ".$User->id.", ".time().", '".$reason."', '' )");
@@ -132,57 +120,54 @@ if ($gameID != 0)
 	
 	if ($Game->Members->isJoined())
 	{
-		if (in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2)))
-        {			
+		if (in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2))) {			
             $countryID1=array($Game->Members->ByUserID[$User->id]->countryID);
         }	
-
-        elseif (in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1)))
-        {
+        elseif (in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1))) {
             $countryID2=array($Game->Members->ByUserID[$User->id]->countryID);
         }
-
-        elseif (!(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2)) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1)))
-        {
+        elseif (!(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID2)) && !(in_array($Game->Members->ByUserID[$User->id]->countryID,$countryID1))) {
             $countryID1=$countryID2=array();
         }
-
-        else
-        {
+        else {
             $countryID1=$countryID2=array($Game->Members->ByUserID[$User->id]->countryID);
         }
 	}
 	
+	print '<div class = "modTools">';
 	print $Game->titleBar().'<br>';
+	print '</div>';
 	
 	print '<DIV class="variant'.$Variant->name.'">
-		<TABLE>
+		<TABLE class = "modTools">
 			<THEAD>
-				<TH style="border: 1px solid #000">Country</TH>
-				<TH style="border: 1px solid #000">Player</TH>
-				<TH style="border: 1px solid #000" align="center">Country 1</TH>
-				<TH style="border: 1px solid #000" align="center">Country 2</TH>
+				<TH class = "modTools" style="border: 1px solid #000">Country</TH>
+				<TH class = "modTools" style="border: 1px solid #000">Player</TH>
+				<TH class = "modTools" style="border: 1px solid #000" align="center">Country 1</TH>
+				<TH class = "modTools" style="border: 1px solid #000" align="center">Country 2</TH>
 			</THEAD>
 			<TR>
-				<TD style="border: 1px solid #666">Global</TD>
-				<TD style="border: 1px solid #666"></TD>
-				<TD style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID1[]" value="0" '.(in_array('0',$countryID1)?'checked':'').'></TD>
-				<TD style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID2[]" value="0" '.(in_array('0',$countryID2)?'checked':'').'></TD>
+				<TD class = "modTools" style="border: 1px solid #666">Global</TD>
+				<TD class = "modTools" style="border: 1px solid #666"></TD>
+				<TD class = "modTools" style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID1[]" value="0" '.(in_array('0',$countryID1)?'checked':'').'></TD>
+				<TD class = "modTools" style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID2[]" value="0" '.(in_array('0',$countryID2)?'checked':'').'></TD>
 			</TR>';
 
 	foreach($Game->Members->ByCountryID as $Member)
+	{
 		print '
 			<TR>
-				<TD style="border: 1px solid #666"><span class="memberCountryName">'.$Member->memberCountryName().'</TD>
-				<TD style="border: 1px solid #666">'.$Member->memberUserDetail().'</TD>
-				<TD style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID1[]" value="'.$Member->countryID.'" '.(in_array($Member->countryID,$countryID1)?'checked':'').'></TD>
-				<TD style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID2[]" value="'.$Member->countryID.'" '.(in_array($Member->countryID,$countryID2)?'checked':'').'></TD>
+				<TD class = "modTools" style="border: 1px solid #666"><span class="memberCountryName">'.$Member->memberCountryName().'</TD>
+				<TD class = "modTools" style="border: 1px solid #666">'.$Member->memberUserDetail().'</TD>
+				<TD class = "modTools" style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID1[]" value="'.$Member->countryID.'" '.(in_array($Member->countryID,$countryID1)?'checked':'').'></TD>
+				<TD class = "modTools" style="border: 1px solid #666" align="center"><input type="checkbox" name="countryID2[]" value="'.$Member->countryID.'" '.(in_array($Member->countryID,$countryID2)?'checked':'').'></TD>
 			</TR>';
+	}
 				
 	print '</TABLE></DIV>';
 }
 
-print '<br><input type="submit" name="Submit" class="form-submit" value="Check" /></form>';
+print '<br><input type="submit" name="Submit" class="modToolsform-submit" value="Check" /></form>';
 
 if (($gameID != 0) && (count($countryID1) > 0) && (count($countryID2) > 0))
 {
@@ -199,19 +184,22 @@ if (($gameID != 0) && (count($countryID1) > 0) && (count($countryID2) > 0))
 	$tabl = $DB->sql_tabl($sql);
 	
 	print '<BR><DIV class="variant'.$Variant->name.'">';
-	print '<TABLE class="chatbox">';
+	print '<TABLE class="modTools">';
 	
-    $alternate = false;
     $output = false;
 	
+	print '<THEAD>';
+	print '	<TH class = "modTools" style="border: 1px solid #000">Time</TH>';
+	print '	<TH class = "modTools" style="border: 1px solid #000">Message</TH>';
+	print '	</THEAD>';
+
 	while ( list($message, $to, $from, $turn, $timeSent) = $DB->tabl_row($tabl) )
 	{
-		$alternate = ! $alternate;
 		$output=true;
-		
-		print '<TR class="replyalternate'.($alternate ? '1' : '2' ).' gameID'.$gameID.'countryID'.$from.'">
-					<TD class="left time">'.libTime::text($timeSent).'</TD>
-					<TD class="right '.( $from != 0 ? 'country'.$from:'').'">
+
+		print '<TR class="modTools">
+					<TD class="modTools">'.libTime::text($timeSent).'</TD>
+					<TD class="'.( $from != 0 ? 'country'.$from:'').'">
 						To: <strong>'.($to==0?'Global':$Game->Variant->countries[$to-1]).'</strong> from <strong>'.($from==0?'Global':$Game->Variant->countries[$from-1]).'</strong>
 						('.$Game->datetxt($turn).'): 
 						<BR>'.$message.'
@@ -236,14 +224,10 @@ if (($gameID != 0) && (count($countryID1) > 0) && (count($countryID2) > 0))
         }
 			
 		print  '<input type="hidden" name="gameID" value="'.$gameID.'">
-				<input type="submit" name="CSV" value="Download as .csv (Excel)">
-				<input type="submit" name="TXT" value="Download as .txt">
+				<input class = "modToolsform-submit" type="submit" name="CSV" value="Download as .csv (Excel)">
+				<input class = "modToolsform-submit" type="submit" name="TXT" value="Download as .txt">
 				</FORM>';
     }
-    
-    else
-    {
-        print '<br>No sent messages to display.';
-    }
+    else { print '<br>No sent messages to display.'; }
 }
 ?>
