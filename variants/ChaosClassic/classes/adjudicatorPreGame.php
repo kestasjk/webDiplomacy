8f03ae84d714453de8c358e4254024f528ac66d7
Daniel Bak <danie01@hitachi-id.com>
2019-03-20 11:49:31 -0600

    Converting spaces > tabs to conform to project standard

--- variants/ChaosClassic/classes/adjudicatorPreGame.php
+++ variants/ChaosClassic/classes/adjudicatorPreGame.php
@@ -3,52 +3,52 @@
 class ClassicChaosVariant_adjudicatorPreGame extends CustomStartVariant_adjudicatorPreGame {
 
 	protected $countryUnits = array(
-        // Britain (1 to 3)
-        'Edinburgh' => array(),
-        'Liverpool' => array(),
-        'London' => array(),
-        // Iberia (4 to 5)
-        'Portugal' => array(),
-        'Spain' => array(),
-        // Tunis (6)
-        'Tunis' => array(),
-        // Italy (7 to 9)
-        'Naples' => array(),
-        'Rome' => array(),
-        'Venice' => array(),
-        // Balkans (10 to 13)
-        'Greece' => array(),
-        'Serbia' => array(),
-        'Bulgaria' => array(),
-        'Rumania' => array(),
-        // Turkey (14 to 16)
-        'Constantinople' => array(),
-        'Smyrna' => array(),
-        'Ankara' => array(),
-        // Russia (17 to 20)
-        'Sevastopol' => array(),
-        'Warsaw' => array(),
-        'Moscow' => array(),
-        'St. Petersburg' => array(),
-        // Scandinavia (21 to 23)
-        'Sweden' => array(),
-        'Norway' => array(),
-        'Denmark' => array(),
-        // Germany (24 to 26)
-        'Kiel' => array(),
-        'Berlin' => array(),
-        'Munich' => array(),
-        // Benelux (27 to 28)
-        'Holland' => array(),
-        'Belgium' => array(),
-        // France (29 to 31)
-        'Brest' => array(),
-        'Paris' => array(),
-        'Marseilles' => array(),
-        // Austria  (32 to 34)
-        'Vienna' => array(),
-        'Trieste' => array(),
-        'Budapest' => array()
+		// Britain (1 to 3)
+		'Edinburgh' => array(),
+		'Liverpool' => array(),
+		'London' => array(),
+		// Iberia (4 to 5)
+		'Portugal' => array(),
+		'Spain' => array(),
+		// Tunis (6)
+		'Tunis' => array(),
+		// Italy (7 to 9)
+		'Naples' => array(),
+		'Rome' => array(),
+		'Venice' => array(),
+		// Balkans (10 to 13)
+		'Greece' => array(),
+		'Serbia' => array(),
+		'Bulgaria' => array(),
+		'Rumania' => array(),
+		// Turkey (14 to 16)
+		'Constantinople' => array(),
+		'Smyrna' => array(),
+		'Ankara' => array(),
+		// Russia (17 to 20)
+		'Sevastopol' => array(),
+		'Warsaw' => array(),
+		'Moscow' => array(),
+		'St. Petersburg' => array(),
+		// Scandinavia (21 to 23)
+		'Sweden' => array(),
+		'Norway' => array(),
+		'Denmark' => array(),
+		// Germany (24 to 26)
+		'Kiel' => array(),
+		'Berlin' => array(),
+		'Munich' => array(),
+		// Benelux (27 to 28)
+		'Holland' => array(),
+		'Belgium' => array(),
+		// France (29 to 31)
+		'Brest' => array(),
+		'Paris' => array(),
+		'Marseilles' => array(),
+		// Austria  (32 to 34)
+		'Vienna' => array(),
+		'Trieste' => array(),
+		'Budapest' => array()
 		);
 
 }
--- variants/ChaosClassic/variant.php
+++ variants/ChaosClassic/variant.php
@@ -9,47 +9,47 @@ class ChaosClassic extends BuildAnywhereVariant {
 	//public $mapID=1;
 	public $name='ChaosClassic';
 	public $fullName='Chaos Classic';
-    public $description='Chaos Classic';
+	public $description='Chaos Classic';
 
-    public $countries=array(
-        'Edinburgh',
-        'Liverpool',
-        'London',
-        'Portugal',
-        'Spain',
-        'Tunis',
-        'Naples',
-        'Rome',
-        'Venice',
-        'Greece',
-        'Serbia',
-        'Bulgaria',
-        'Rumania',
-        'Constantinople',
-        'Smyrna',
-        'Ankara',
-        'Sevastopol',
-        'Warsaw',
-        'Moscow',
-        'St. Petersburg',
-        'Sweden',
-        'Norway',
-        'Denmark',
-        'Kiel',
-        'Berlin',
-        'Munich',
-        'Holland',
-        'Belgium',
-        'Brest',
-        'Paris',
-        'Marseilles',
-        'Vienna',
-        'Trieste',
-        'Budapest'
+	public $countries=array(
+		'Edinburgh',
+		'Liverpool',
+		'London',
+		'Portugal',
+		'Spain',
+		'Tunis',
+		'Naples',
+		'Rome',
+		'Venice',
+		'Greece',
+		'Serbia',
+		'Bulgaria',
+		'Rumania',
+		'Constantinople',
+		'Smyrna',
+		'Ankara',
+		'Sevastopol',
+		'Warsaw',
+		'Moscow',
+		'St. Petersburg',
+		'Sweden',
+		'Norway',
+		'Denmark',
+		'Kiel',
+		'Berlin',
+		'Munich',
+		'Holland',
+		'Belgium',
+		'Brest',
+		'Paris',
+		'Marseilles',
+		'Vienna',
+		'Trieste',
+		'Budapest'
 		)
-    //public $author='Avalon Hill';
-    
-    public function __construct() {
+	//public $author='Avalon Hill';
+	
+	public function __construct() {
 		parent::__construct();
 
 		// Order validation code, changed to validate builds on non-home SCs
@@ -57,13 +57,13 @@ class ChaosClassic extends BuildAnywhereVariant {
 
 		// Order interface/generation code, changed to add javascript in resources which makes non-home SCs an option
 		$this->variantClasses['OrderInterface'] = 'BuildAnywhere';
-    		// Altered to disable the creation of starting units
+			// Altered to disable the creation of starting units
 		$this->variantClasses['adjudicatorPreGame'] = 'ChaosClassic';
 
 		// Altered to change the starting order of a game's phases; Spring 1901 Pre-game|Unit-placing|Diplomacy|Retreats ->
-        $this->variantClasses['processGame'] = 'CustomStart';
-        
-        $this->variantClasses['drawMap'] = 'ChaosClassic';
+		$this->variantClasses['processGame'] = 'CustomStart';
+		
+		$this->variantClasses['drawMap'] = 'ChaosClassic';
 
 	}
 }
