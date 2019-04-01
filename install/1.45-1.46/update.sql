5968396ebe1bbdf9d404ecd3b6d3bf513fc6a229
jmo1121109 <jmo1121109@users.noreply.github.com>
2019-03-31 20:04:12 -0400

    Add new column to Backup table too
    The backup table isn't referenced in the code directly but is somehow being used on leaving games and needs the same columns.

--- install/1.45-1.46/update.sql
+++ install/1.45-1.46/update.sql
@@ -1,3 +1,4 @@
 UPDATE `wD_Misc` SET `value` = '146' WHERE `name` = 'Version';
 
 ALTER TABLE `wD_Members` ADD `hideNotifications` boolean DEFAULT false;
+ALTER TABLE `wD_Backup_Members` ADD `hideNotifications` boolean DEFAULT false;
