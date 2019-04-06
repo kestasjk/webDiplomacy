cc9f9bd7f3e36189be2fc4b0a0da98510c8f681e
jmo1121109 <webdipgit@gmail.com>
2019-04-05 21:05:56 -0400

    Fix Home Spectate Option
    Remove "stop spectating" as an option for games you aren't in.

--- gamepanel/gamehome.php
+++ gamepanel/gamehome.php
@@ -168,7 +168,7 @@ class panelGameHome extends panelGameBoard
 
 		if ($this->watched() || $userInGame == 0)
 		{
-			$watchString = '- <a href="board.php?gameID='.$this->id.'&unwatch">'.l_t('Stop spectating').'</a>';
+			if ($this->watched()) { $watchString = '- <a href="board.php?gameID='.$this->id.'&unwatch">'.l_t('Stop spectating').'</a>'; }
 			if( $this->phase == 'Pre-game')
 			{
 				return '<div class="bar homeGameLinks barAlt'.libHTML::alternate().'">
