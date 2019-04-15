6c45c6139d2e623c6f2c455b604485d5563b6e9b
Spencer Dant <sdant@Spencers-MacBook-Pro.local>
2019-04-15 17:07:33 -0500

    fixed header css, removed ES6 from js because some people still use IE

--- javascript/clickHandler.js
+++ javascript/clickHandler.js
@@ -1,8 +1,8 @@
-const headerClick = document.getElementsByClassName('clickable');
+var headerClick = document.getElementsByClassName('clickable');
 function click(e) {
 	if (e.currentTarget.hasChildNodes()) {
 		if (e.currentTarget.children[0].style.visibility == 'hidden' || e.currentTarget.children[0].style.visibility == '') {
-			for (let i = 0; i < headerClick.length; i++){
+			for (var i = 0; i < headerClick.length; i++){
 				if (headerClick[i].children[0].style.visibility == 'visible') {
 					headerClick[i].children[0].style.visibility = 'hidden'
 				} 
