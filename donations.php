39dd39ef2872d61d61a592e0ee38660a7a2e77a7
Spencer Dant <shdant113@gmail.com>
2019-04-20 15:32:04 -0400

    amended and added content to donations page

--- donations.php
+++ donations.php
@@ -30,31 +30,47 @@ print libHTML::pageTitle(l_t('Donations'),l_t('Learn how to donate and what your
 
 print "
 <div class='donations'>
-    <div class = 'donations_title'>Why do we need donations?</div>
+    <div class = 'donations_title'>Why do you need donations?</div>
     <div class = 'donations_content'>
         <p>
-            We offer the game Diplomacy free of charge. Our goal is to be the best place for you to play the game of diplomacy online without ever bothering you with ads. 
-            To do this we need occasional support from you. 
+            webDiplomacy offers you the chance to play Diplomacy online free of charge. Our goal is to always be the best place for you to play the game of diplomacy online without 
+            ever bothering you with ads, charging you for extra features, or charging you to play games. To do this we need occasional support from you. 
         </p>
     </div>
 
-    <div class = 'donations_title'>What do we use your donations for?</div>
+    <div class = 'donations_title'>How do you use my donation?</div>
     <div class = 'donations_content'>
         <p>
-           Donations are mainly used to pay for the cost of the server. Leftover funds are used for development of new features and functionality. The owners, 
-           admins, and moderators do not take a salary and never will. Your contribution is only ever used for the site.
+           Donations are mainly used to pay for the cost of the server, domain, and maintenance costs. The owners, admins, and moderators do not take a salary and never will. 
+           Leftover funds may be used for development of features such as our email service to confirm new members in bulk. 
+           Your contribution is only ever used for the betterment of webDiplomacy.
         </p>
     </div>
 
-    <div class = 'donations_title'>What do get for donating?</div>
+    <div class = 'donations_title'>Why don't you get ad revenue, sell user data, or charge for premium features?</div>
     <div class = 'donations_content'>
         <p>
-           We will give you a marker on your profile and on the forum to signifiy your support for the site. 
-           However we never want our members to feel obligated to contribute so no functionality is locked behind a paywall. 
+           We do not believe that it is right to distract you with obnoxious advertisements, sell your private data to third parties, or charge you for extra features. We are an 
+           entirely free, safe-to-use site and always will be.
         </p>
     </div>
 
-    <div class = 'donations_title'>How do you donate?</div>
+    <div class = 'donations_title'>What do I get for donating?</div>
+    <div class = 'donations_content'>
+        <p>
+           We will give you a marker on your profile and on the forum to signifiy your support for the site. However we never want our members to feel obligated to contribute so no 
+           functionality is locked behind a paywall, nor do we ever discriminate in any way against those who have not donated. 
+        </p>
+    </div>
+
+    <div class = 'donations_title'>Do I have to donate?</div>
+    <div class = 'donations_content'>
+        <p>
+           No, donations are optional. We will never require a user to donate in order to gain any perks, play more games, or enjoy our full site.
+        </p>
+    </div>
+
+    <div class = 'donations_title'>How do I donate?</div>
     <div class = 'donations_content_show'>
         <p>
             If you would like to support the site, click the button below. After submitting a donation, please send an email to a Co-owner at <a href='mailto:".Config::$adminEMail."' class='light'>".Config::$adminEMail."</a> with your username to receive a donator marker. </br>
