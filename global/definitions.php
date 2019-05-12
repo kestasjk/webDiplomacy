20253acad97a95c028eda62aace8b395c3af15fe
jmo1121109 <webdipgit@gmail.com>
2019-05-12 19:44:34 -0400

    Update css version
    cachebusting

--- global/definitions.php
+++ global/definitions.php
@@ -47,7 +47,7 @@ define("CSSDIR", 'css');
 
 //Increment these versions whenever you update any js or css files for cachebusting
 define("JSVERSION",1.2);
-define("CSSVERSION",1.9);
+define("CSSVERSION",1.10);
 
 if( !defined('FACEBOOK') )
 	define('FACEBOOK',false);
