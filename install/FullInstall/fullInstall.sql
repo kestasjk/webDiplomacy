a0c86e83848d39c69cf3e29a36804e69505a0690
Squigs44 <96dwilliams@gmail.com>
2019-05-14 17:59:46 -0600

    Also changed the backup games table, in case we want to save a bigshot game

--- install/FullInstall/fullInstall.sql
+++ install/FullInstall/fullInstall.sql
@@ -822,3 +822,5 @@ UPDATE `wD_Misc` SET `value` = '152' WHERE `name` = 'Version';
 
 ALTER TABLE 'wD_Games' MODIFY 'pot' MEDIUMINT(8);
 ALTER TABLE 'wD_Games' MODIFY 'minimumBet' MEDIUMINT(8);
+ALTER TABLE 'wD_Backup_Games' MODIFY 'pot' MEDIUMINT(8);
+ALTER TABLE 'wD_Backup_Games' MODIFY 'minimumBet' MEDIUMINT(8);
