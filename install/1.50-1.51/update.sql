ad131f9f9d31904e7bfa530d06f515ab69cb3c97
guitargun <1guitargun1@gmail.com>
2019-05-23 16:41:56 -0230

    missing semicolumn causing error while trying to update

--- install/1.50-1.51/update.sql
+++ install/1.50-1.51/update.sql
@@ -4,6 +4,6 @@ CREATE TABLE `wD_Config` (
   `name` enum('Notice','Panic','Maintenance') NOT NULL,
   `message` text NOT NULL,
   PRIMARY KEY (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 INSERT INTO wD_Config VALUES ('Notice','Default server-wide notice message.'),('Panic','Game processing has been paused and user registration has been disabled while a problem is resolved.'),('Maintenance','Server is in maintenance mode; only admins can fully interact with the server.'),('ServerOffline','');
