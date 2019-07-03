545ae8893dd4e2369f555f34375ae57b836948cc
Spencer Dant <shdant113@gmail.com>
2019-07-03 15:39:59 -0400

    ancient med

--- variants/World/classes/Chatbox.php
+++ variants/World/classes/Chatbox.php
@@ -8,9 +8,9 @@ class WorldVariant_Chatbox extends Chatbox {
 		for($i=0; $i<count($messages); $i++)
 			if( $messages[$i]['fromCountryID']!=0)
 				if (!isset($Member) || $Member->countryID != $messages[$i]['fromCountryID'])
-					$messages[$i]['message'] = '[<strong>'.$this->countryName($messages[$i]['fromCountryID']).'</strong>]<span style="color: black;">:'.$messages[$i]['message'];
+					$messages[$i]['message'] = '[<strong>'.$this->countryName($messages[$i]['fromCountryID']).'</strong>]<span style="color: rgba(255, 255, 255, 0.8);">:'.$messages[$i]['message'];
 				else
-					$messages[$i]['message'] = '[<strong>You</strong>]<span style="color: black;">:'.$messages[$i]['message'];				
+					$messages[$i]['message'] = '[<strong>You</strong>]<span style="color: rgba(255, 255, 255, 0.8);">:'.$messages[$i]['message'];				
 
 		return parent::renderMessages($msgCountryID, $messages);
 
