8d25fb0e88a6eadae8cc1f671ba61d3aca78d29f
Squigs44 <96dwilliams@gmail.com>
2019-08-05 01:02:31 -0600

    Database changes in preparation for Ghost Rating integration

--- install/FullInstall/fullInstall.sql
+++ install/FullInstall/fullInstall.sql
@@ -854,3 +854,38 @@ CREATE TABLE `wD_ApiPermissions` (
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 ALTER TABLE `wD_ApiPermissions` ADD CONSTRAINT `wD_ApiPermissions_pk` PRIMARY KEY (`userID`);
+
+UPDATE `wD_Misc` SET `value` = '156' WHERE `name` = 'Version';
+
+ALTER TABLE `wD_MissedTurns` ADD `liveGame` BOOLEAN DEFAULT 0;  
+
+update wD_MissedTurns m inner join wD_Games g on g.id = m.gameID set m.liveGame = 1 where g.phaseMinutes < 61 and g.id is not null;
+
+UPDATE `wD_Misc` SET `value` = '157' WHERE `name` = 'Version';
+
+ALTER TABLE `wD_UserOptions` ADD `darkMode` enum('Yes','No') NOT NULL DEFAULT 'No';
+
+UPDATE `wD_Misc` SET `value` = '158' WHERE `name` = 'Version';
+
+CREATE TABLE `wD_VariantInfo` (
+  `variantID` smallint(4) unsigned NOT NULL,
+  `mapID` smallint(4) unsigned NOT NULL,
+  `supplyCenterTarget` smallint(4) unsigned NOT NULL,
+  `supplyCenterCount` smallint(4) unsigned NOT NULL,
+  `countryCount` smallint(4) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `fullName` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `adapter` varchar(50),
+  `version` varchar(10),
+  `codeVersion` varchar(10),
+  `homepage` varchar(100),
+  `countriesList` varchar(800) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `wD_Games` ADD `finishTime` int(10) unsigned DEFAULT NULL;
+
+UPDATE `wD_Games` g SET `finishTime` = (SELECT MAX(n.timeSent) FROM `wD_Notices` n WHERE n.type = 'Games' AND n.linkID = g.id) WHERE (SELECT COUNT(1) FROM `wD_Notices` n1 WHERE n1.linkID = g.id AND `type` = 'Games') > 0;
+
+UPDATE `wD_Games` SET `finishTime` = `processTime` WHERE `finishTime` IS NULL;
