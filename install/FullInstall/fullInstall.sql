a8434052973f7bcdd3fb79ab4d99564c56362dc2
Squigs44 <41693499+Squigs44@users.noreply.github.com>
2019-08-12 17:46:25 -0600

    Update fullInstall.sql

--- install/FullInstall/fullInstall.sql
+++ install/FullInstall/fullInstall.sql
@@ -902,6 +902,7 @@ ALTER TABLE `wD_Users`
 CHANGE `type` `type` SET(
 	'Banned', 'Guest', 'System', 'User', 'Moderator',
 	'Admin', 'Donator', 'DonatorBronze', 'DonatorSilver',
-	'DonatorGold', 'DonatorPlatinum', 'ForumModerator', 'Bot'
+	'DonatorGold', 'DonatorPlatinum', 'ForumModerator', 'FtfTD',
+	'DonatorAdamantium', 'DonatorService', 'DonatorOwner', 'Bot'
 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'User';
 
