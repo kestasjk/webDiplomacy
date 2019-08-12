08eaefc533e33def4687283f59fd8c22471c7152
Squigs44 <41693499+Squigs44@users.noreply.github.com>
2019-08-12 17:45:45 -0600

    Update update.sql

--- install/1.58-1.59/update.sql
+++ install/1.58-1.59/update.sql
@@ -4,5 +4,6 @@ ALTER TABLE `wD_Users`
 CHANGE `type` `type` SET(
 	'Banned', 'Guest', 'System', 'User', 'Moderator',
 	'Admin', 'Donator', 'DonatorBronze', 'DonatorSilver',
-	'DonatorGold', 'DonatorPlatinum', 'ForumModerator', 'Bot'
+	'DonatorGold', 'DonatorPlatinum', 'ForumModerator', 'FtfTD',
+	'DonatorAdamantium', 'DonatorService', 'DonatorOwner', 'Bot'
 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'User';
