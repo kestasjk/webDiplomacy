f96a15c0c6ec6006e4f413e13b29950d416acbb5
Spencer Dant <shdant113@gmail.com>
2019-09-21 10:16:02 -0500

    faq section/credit for bots, dark mode PM system fixed

--- global/definitions.php
+++ global/definitions.php
@@ -48,7 +48,7 @@ define("CSSDIR", 'css');
 
 //Increment these versions whenever you update any js or css files for cachebusting
 define("JSVERSION",1.4);
-define("CSSVERSION",1.32);
+define("CSSVERSION",1.33);
 
 if( !defined('FACEBOOK') )
 	define('FACEBOOK',false);
