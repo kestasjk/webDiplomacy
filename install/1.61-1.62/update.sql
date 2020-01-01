3f17073a369bb7528e5d8d545356d9b78fb0d88e
Restitution-Dip <holesquared@protonmail.com>
2019-12-31 17:00:11 -0700

    Extended changes to wd_backup_games.

--- install/1.61-1.62/update.sql
+++ install/1.61-1.62/update.sql
@@ -8,3 +8,13 @@ UPDATE wD_Games
 SET nextPhaseMinutes = phaseMinutes
 WHERE nextPhaseMinutes = 0;
 
+
+ALTER TABLE `wD_Backup_Games`
+ADD COLUMN `nextPhaseMinutes` int(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `phaseMinutes`,
+ADD COLUMN `phaseSwitchPeriod` int(10) DEFAULT -1 AFTER `nextPhaseMinutes`,
+ADD COLUMN `createTime` int(10) UNSIGNED DEFAULT NULL,
+ADD COLUMN `startTime` int(10) UNSIGNED DEFAULT NULL;
+
+UPDATE wD_Backup_Games
+SET nextPhaseMinutes = phaseMinutes
+WHERE nextPhaseMinutes = 0;
