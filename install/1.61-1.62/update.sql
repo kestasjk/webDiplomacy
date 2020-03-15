7cde71b07c677fd43183e3827a05d88640ed41ee
Restitution-Dip <holesquared@protonmail.com>
2020-03-15 17:35:08 -0600

    Added missing column

--- install/1.61-1.62/update.sql
+++ install/1.61-1.62/update.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `wD_Games`
-ADD COLUMN `nextPhaseMinutes` int(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `phaseMinutes`
+ADD COLUMN `nextPhaseMinutes` int(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `phaseMinutes`,
 ADD COLUMN `phaseSwitchPeriod` int(10) DEFAULT -1 AFTER `nextPhaseMinutes`,
 ADD COLUMN `startTime` int(10) UNSIGNED DEFAULT NULL;
 
