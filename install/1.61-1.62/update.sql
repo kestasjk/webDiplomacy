ade60021d2ec5cd39d3157d74519b1a72500c095
Restitution-Dip <53350320+Restitution-Dip@users.noreply.github.com>
2020-03-17 10:47:20 -0600

    Updating wD_Misc to 162

--- install/1.61-1.62/update.sql
+++ install/1.61-1.62/update.sql
@@ -1,3 +1,5 @@
+UPDATE `wD_Misc` SET `value` = '162' WHERE `name` = 'Version';
+
 ALTER TABLE `wD_Games`
 ADD COLUMN `nextPhaseMinutes` int(10) UNSIGNED DEFAULT 0 NOT NULL AFTER `phaseMinutes`,
 ADD COLUMN `phaseSwitchPeriod` int(10) DEFAULT -1 AFTER `nextPhaseMinutes`,
