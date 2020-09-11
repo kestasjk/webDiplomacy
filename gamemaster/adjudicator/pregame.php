37f2da353488c900a367fabac5009153669781db
jmo1121109 <webdipgit@gmail.com>
2020-09-10 23:42:04 -0400

    Update Git to Match Server
    Update with various php 7+ Kestas made on the server.

--- gamemaster/adjudicator/pregame.php
+++ gamemaster/adjudicator/pregame.php
@@ -225,7 +225,7 @@ class adjudicatorPreGame
 		}
 
 		// Determine which countryID is given to which userID
-		if (count($Game->Members->ByCountryID)==0)
+		if ( empty($Game->Members->ByCountryID) || ( is_array($Game->Members->ByCountryID) && count($Game->Members->ByCountryID) == 0))
 		{
 			$userCountries = $this->userCountries();// $userCountries[$userID]=$countryID
 			assert('count($userCountries) == count($Game->Variant->countries) && count($userCountries) == count($Game->Members->ByID)');
