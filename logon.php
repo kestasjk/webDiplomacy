2d0783f5842c3aca1a8d0f1bc46104ebd121d39d
Kestas Kuliukas <kestas.j.k@gmail.com>
2021-07-23 20:32:00 +0800

    Typo fix

--- logon.php
+++ logon.php
@@ -58,13 +58,13 @@ if( isset($_REQUEST['forgotPassword']) and $User->type['Guest'] )
 					"<a href='logon.php?forgotPassword=1' class='light'>go back</a> and check your spelling."));
 			}
 
-			if( $MC->get('forgot_'+$forgottenUser->id) !== false )
+			if( $MC->get('forgot_'.$forgottenUser->id) !== false )
 			{
 				throw new Exception(l_t("To help prevent abuse please wait 5 minutes before resending forgotten e-mail recovery links. ".
 					"In the meantime please check your spam folder for a missing recovery e-mail, or contact the moderator team."));
 			}
 			
-			$MC->set('forgot_'+$forgottenUser->id, 5*60); // Set a flag preventing resends for 5 minutes
+			$MC->set('forgot_'.$forgottenUser->id, 5*60); // Set a flag preventing resends for 5 minutes
 
 			require_once(l_r('objects/mailer.php'));
 			$Mailer = new Mailer();
