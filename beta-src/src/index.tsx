be1f27b4c2b3466b6491d17e765d8726d96182aa
Steve Benedict <sbenedict@fb.com>
2022-01-19 17:48:11 -0800

    added mui to the project and some usage to prove it works

--- beta-src/src/index.tsx
+++ beta-src/src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/css/index.css";
+import { CssBaseline } from "@mui/material";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
