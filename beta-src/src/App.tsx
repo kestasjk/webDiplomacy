56345a461b01bed554b313acb2421a065dbbda76
Steve Benedict <sbenedict@fb.com>
2022-01-27 08:54:54 -0800

    further changes to theme

--- beta-src/src/App.tsx
+++ beta-src/src/App.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
 import "./assets/css/App.css";
-import Box from "@mui/material/Box";
-import map from "./assets/svg/map.svg";
+import { Box, Typography } from "@mui/material";
+// import map from "./assets/svg/map.svg"; <img alt="Game Map" src={map} />
 
 const App: React.FC = function (): React.ReactElement {
   return (
     <Box className="App">
-      <img alt="Game Map" src={map} />
+      <Typography gutterBottom variant="h1">
+        variant h1, figma h1
+      </Typography>
+      <Typography gutterBottom variant="h2">
+        variant h2, figma h2
+      </Typography>
+      <Typography gutterBottom variant="h3">
+        variant h3, figma h3
+      </Typography>
+      <Typography gutterBottom variant="body1">
+        variant body1 (default), figma p
+      </Typography>
+      <Typography gutterBottom variant="smallLabel" component="label">
+        variant smallLabel, figma small label
+      </Typography>
     </Box>
   );
 };
--- beta-src/src/webDiplomacyTheme.ts
+++ beta-src/src/webDiplomacyTheme.ts
@@ -1,13 +1,40 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: false;
+    sm: false;
+    md: false;
+    lg: false;
+    xl: false;
+    mobile: true;
+    mobileLg: true;
+    tablet: true;
+    desktop: true;
+  }
+
+  interface TypographyVariants {
+    smallLabel: React.CSSProperties;
+  }
+
+  export interface TypographyVariantsOptions {
+    smallLabel?: React.CSSProperties;
+  }
+}
+
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    smallLabel: true;
+  }
+}
+
 const webDiplomacyTheme = createTheme({
   breakpoints: {
     values: {
-      xs: 0,
-      sm: 414,
-      md: 834,
-      lg: 1200,
-      xl: 1500,
+      mobile: 0,
+      mobileLg: 414,
+      tablet: 834,
+      desktop: 1500,
     },
   },
   palette: {
@@ -25,7 +52,30 @@ const webDiplomacyTheme = createTheme({
     },
   },
   typography: {
-    fontFamily: "SF Pro Display, Segoe UI, Droid Sans, sans-serif",
+    body1: {
+      fontSize: 14,
+      lineHeight: 1.29,
+    },
+    h1: {
+      fontSize: 20,
+      fontWeight: 700,
+      lineHeight: 1.2,
+    },
+    h2: {
+      fontSize: 16,
+      fontWeight: 700,
+      lineHeight: 1.125,
+    },
+    h3: {
+      fontSize: 14,
+      fontWeight: 700,
+      lineHeight: 1.15,
+    },
+    smallLabel: {
+      fontSize: 10,
+      fontWeight: 400,
+      lineHeight: 1,
+    },
   },
 });
 
