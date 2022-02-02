938eab33f0c821999ee3b7177e39395aae52414a
Jin Choi <jinchoi1@fb.com>
2022-02-02 10:48:24 -0800

    app.js change

--- beta-src/src/App.tsx
+++ beta-src/src/App.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import "./assets/css/App.css";
 import Box from "@mui/material/Box";
 import map from "./assets/svg/map.svg";
-import WDActionIcon from "./components/WDActionIcon";
-import WDHomeIcon from "./components/WDHomeIcon";
 
 const App: React.FC = function (): React.ReactElement {
   return (
