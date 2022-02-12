00b4287d8bc53c785331eb3a32481144fcdd6305
Kestas Kuliukas <kestas@kuliukas.com>
2022-02-12 21:15:41 +0800

    Added a new install guide and donation recording

--- install/FullInstall/fullInstall.sql
+++ install/FullInstall/fullInstall.sql
@@ -979,3 +979,28 @@ ADD COLUMN `grCalculated` INT NOT NULL DEFAULT 0,
 ADD INDEX (`grCalculated`);
 
 UPDATE `wD_Misc` SET `value` = '166' WHERE `name` = 'Version';
+
+ALTER TABLE `wD_GameMessages` ADD `phaseMarker` ENUM('Finished','Pre-game','Diplomacy','Retreats','Builds') NULL DEFAULT NULL AFTER `gameID`; 
+ALTER TABLE `wD_Backup_GameMessages` ADD `phaseMarker` ENUM('Finished','Pre-game','Diplomacy','Retreats','Builds') NULL DEFAULT NULL AFTER `gameID`; 
+
+ALTER TABLE `wD_Users` ADD `optInFeatures` int(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `tempBanReason`;
+
+UPDATE `wD_Misc` SET `value` = '167' WHERE `name` = 'Version';
+
+
+ALTER TABLE `wD_GameMessages` ADD `phaseMarker` ENUM('Finished','Pre-game','Diplomacy','Retreats','Builds') NULL DEFAULT NULL AFTER `gameID`; 
+ALTER TABLE `wD_Backup_GameMessages` ADD `phaseMarker` ENUM('Finished','Pre-game','Diplomacy','Retreats','Builds') NULL DEFAULT NULL AFTER `gameID`; 
+
+ALTER TABLE `wD_Users` ADD `optInFeatures` int(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `tempBanReason`;
+
+UPDATE `wD_Misc` SET `value` = '168' WHERE `name` = 'Version';
+CREATE TABLE `wD_PaypalIPN` ( 
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+ `userId` int(10) unsigned NOT NULL, 
+ `email` varchar(250) NOT NULL, 
+ `value` float NOT NULL, 
+ `currency` varchar(10) NOT NULL, 
+ `status` varchar(100) NOT NULL, 
+ `receivedTime` bigint(20) unsigned NOT NULL, 
+ PRIMARY KEY (`id`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
