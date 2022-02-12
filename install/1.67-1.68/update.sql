11db01a16aa7e0cbbafd097ce50c2c7ebd9a721d
Kestas Kuliukas <kestas@kuliukas.com>
2022-02-12 21:27:43 +0800

    Added missing table to 1.68 script

--- install/1.67-1.68/update.sql
+++ install/1.67-1.68/update.sql
@@ -10,3 +10,17 @@ CREATE TABLE `wD_PaypalIPN` (
  `receivedTime` bigint(20) unsigned NOT NULL, 
  PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `wD_GameMessages_Redacted` (                                                                                                                                             
+  `id` bigint(20) unsigned NOT NULL,                                                                                                                                                  
+  `timeSent` int(10) unsigned NOT NULL,                                                                                                                                               
+  `message` text NOT NULL,                                                                                                                                                            
+  `turn` smallint(5) unsigned NOT NULL,                                                                                                                                               
+  `toCountryID` tinyint(3) unsigned NOT NULL,                                                                                                                                         
+  `fromCountryID` tinyint(3) unsigned NOT NULL,                                                                                                                                       
+  `gameID` mediumint(8) unsigned NOT NULL,                                                                                                                                            
+  `phaseMarker` enum('Finished','Pre-game','Diplomacy','Retreats','Builds') DEFAULT NULL,                                                                                             
+  PRIMARY KEY (`id`) USING BTREE,                                                                                                                                                     
+  KEY `toMember` (`gameID`,`toCountryID`) USING BTREE,                                                                                                                                
+  KEY `fromMember` (`gameID`,`fromCountryID`) USING BTREE                                                                                                                             
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;                  
