e06ee9f9c49673ea02068878ba5e6491a5474256
Steve Benedict <sbenedict@fb.com>
2022-02-18 09:28:30 -0800

    first pass at position container

---
+++ beta-src/src/components/ui/WDPositionContainer.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { useCallback } from "react";
+
+export enum Position {
+  BOTTOM_LEFT = "bottom-left",
+  BOTTOM_RIGHT = "bottom-right",
+  TOP_LEFT = "top-left",
+  TOP_RIGHT = "top-right",
+}
+
+interface WDPositionContainerProps {
+  children: React.ReactNode;
+  position?: Position;
+}
+
+const MOBILE_DISTANCE = 16;
+const TABLET_UP_DISTANCE = 24;
+
+const responsiveDistance = {
+  mobile: MOBILE_DISTANCE,
+  tablet: TABLET_UP_DISTANCE,
+  desktop: TABLET_UP_DISTANCE,
+} as const;
+
+/**
+ * This component is used to position a box and its children absolute
+ */
+const WDPositionContainer: React.FC<WDPositionContainerProps> = function ({
+  children,
+  position,
+}): React.ReactElement {
+  const getPositionValues = useCallback(() => {
+    switch (position) {
+      case Position.BOTTOM_LEFT:
+        return { bottom: responsiveDistance, left: responsiveDistance };
+      case Position.BOTTOM_RIGHT:
+        return { bottom: responsiveDistance, right: responsiveDistance };
+      case Position.TOP_RIGHT:
+        return { right: responsiveDistance, top: responsiveDistance };
+      case Position.TOP_LEFT:
+      default:
+        return { left: responsiveDistance, top: responsiveDistance };
+    }
+  }, [position]);
+
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        zIndex: 2,
+        ...getPositionValues(),
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
+
+WDPositionContainer.defaultProps = {
+  position: Position.TOP_LEFT,
+};
+
+export default WDPositionContainer;
