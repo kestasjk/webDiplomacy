f219f94e86928c7c83550ed72332cca1c1d5b672
kenneth-yf-wang <kwang@codazen.com>
2022-03-02 12:43:36 -0800

    correct alpha order and replaced div with Box

--- beta-src/src/components/ui/WDInfoDisplay.tsx
+++ beta-src/src/components/ui/WDInfoDisplay.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {
+  Box,
   Table,
   TableBody,
   TableCell,
@@ -13,68 +14,68 @@ import {
  */
 
 interface WDInfoDisplayProps {
-  title: string;
-  gameTime: string;
-  phase: string;
-  season: string;
-  year: string;
+  gameTime: number;
   gameType: string;
+  phase: string;
   playType: string;
   rank: string;
+  season: string;
+  title: string;
+  year: string;
 }
 
+const tableCellStyles = {
+  border: "none",
+  fontSize: "0.7rem",
+  p: "0px 5px 0px 0px",
+};
+
 const WDInfoDisplay: React.FC<WDInfoDisplayProps> = function ({
-  title,
   gameTime,
-  phase,
-  season,
-  year,
   gameType,
+  phase,
   playType,
   rank,
+  season,
+  title,
+  year,
 }) {
-  const tableCellStyles = {
-    p: "0px 5px 0px 0px",
-    fontSize: "0.7rem",
-    border: "none",
-  };
-
   return (
     <TableContainer>
       <Table
-        sx={{
-          maxWidth: "250px",
-        }}
-        size="small"
         aria-label="a dense table"
+        size="small"
+        sx={{
+          maxWidth: 250,
+        }}
       >
         <TableHead>
           <TableRow>
             <TableCell
               sx={{
+                border: "none",
                 fontWeight: 600,
                 p: "2px 5px 5px 0px",
-                border: "none",
               }}
             >
-              <div
+              <Box
                 /**
                  * by applying CSS properties, the title of the game would be able to wrap until the second line
                  * for the second line display '...' to show it being cut-off
                  */
-                style={{
+                sx={{
+                  display: "-webkit-box",
                   lineHeight: "1.2rem",
                   maxHeight: "2.4rem",
                   overflow: "hidden",
                   textOverflow: "ellipsis",
                   wordWrap: "break-word",
-                  display: "-webkit-box",
                   WebkitLineClamp: 2,
                   WebkitBoxOrient: "vertical",
                 }}
               >
                 {title}
-              </div>
+              </Box>
             </TableCell>
           </TableRow>
         </TableHead>
