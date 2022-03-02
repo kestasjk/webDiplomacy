1d6e82fcbbf37ee2a609a37eb96ddc64f3265a91
Jin Choi <jinchoi1@fb.com>
2022-03-01 16:07:18 -0800

    removed PhaseScroll interface file

--- beta-src/src/interfaces/PhaseScroll.ts
+++
@@ -1,7 +0,0 @@
-import ScrollButtonState from "../enums/ScrollButton";
-
-export interface scrollButtonProps {
-  direction: ScrollButtonState;
-  disabled?: boolean;
-  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
-}
