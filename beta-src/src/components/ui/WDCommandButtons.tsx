da999c83cf0c27e460dcbea9127079a42078d461
kenneth-yf-wang <kwang@codazen.com>
2022-03-11 09:48:50 -0800

    added return type for function, delete comments and update props name

--- beta-src/src/components/ui/WDCommandButtons.tsx
+++ beta-src/src/components/ui/WDCommandButtons.tsx
@@ -5,40 +5,36 @@ import WDButton from "./WDButton";
 import VoteType from "../../types/Vote";
 import Vote from "../../enums/Vote";
 
-/**
- * Define the voteState props passed to the function.
- * Define the setVoteState props passed to the function.
- */
 interface voteProps {
   voteState: VoteType;
-  setState: (vote: Vote) => void;
+  toggleVote: (vote: Vote) => void;
 }
 
 const WDCommandButtons: React.FC<voteProps> = function ({
   voteState,
-  setState,
-}) {
+  toggleVote,
+}): React.ReactElement {
   const { draw, pause, cancel } = voteState;
 
   return (
     <Stack direction="row" spacing={2} alignItems="center">
       <WDButton
         color={draw ? "secondary" : "primary"}
-        onClick={() => setState(Vote.DRAW)}
+        onClick={() => toggleVote(Vote.DRAW)}
         startIcon={draw ? <WDCheckmarkIcon /> : ""}
       >
         Draw
       </WDButton>
       <WDButton
         color={pause ? "secondary" : "primary"}
-        onClick={() => setState(Vote.PAUSE)}
+        onClick={() => toggleVote(Vote.PAUSE)}
         startIcon={pause ? <WDCheckmarkIcon /> : ""}
       >
         Pause
       </WDButton>
       <WDButton
         color={cancel ? "secondary" : "primary"}
-        onClick={() => setState(Vote.CANCEL)}
+        onClick={() => toggleVote(Vote.CANCEL)}
         startIcon={cancel ? <WDCheckmarkIcon /> : ""}
       >
         Cancel
