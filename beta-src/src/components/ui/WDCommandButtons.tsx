7cbf4c7ac734c69e6f33aac4cf6f731c31601692
kenneth-yf-wang <kwang@codazen.com>
2022-03-11 14:46:19 -0800

    applied map to make buttons

--- beta-src/src/components/ui/WDCommandButtons.tsx
+++ beta-src/src/components/ui/WDCommandButtons.tsx
@@ -3,42 +3,37 @@ import { Stack } from "@mui/material";
 import WDCheckmarkIcon from "../svgr-components/WDCheckmarkIcon";
 import WDButton from "./WDButton";
 import VoteType from "../../types/Vote";
-import Vote from "../../enums/Vote";
+import voteStateArray from "../../utils/voteStateArray";
 
 interface voteProps {
   voteState: VoteType;
-  toggleVote: (vote: Vote) => void;
+  toggleVote: (vote: string) => void;
 }
 
 const WDCommandButtons: React.FC<voteProps> = function ({
   voteState,
   toggleVote,
 }): React.ReactElement {
-  const { draw, pause, cancel } = voteState;
+  const voteArray = voteStateArray(voteState);
+
+  const commendButtons = voteArray.map((singleVote) => {
+    const { vote, status } = singleVote;
+    const displayName = vote[0].toUpperCase() + vote.slice(1);
+    return (
+      <WDButton
+        key={vote}
+        color={status ? "secondary" : "primary"}
+        onClick={() => toggleVote(vote)}
+        startIcon={status ? <WDCheckmarkIcon /> : ""}
+      >
+        {displayName}
+      </WDButton>
+    );
+  });
 
   return (
     <Stack direction="row" spacing={2} alignItems="center">
-      <WDButton
-        color={draw ? "secondary" : "primary"}
-        onClick={() => toggleVote(Vote.DRAW)}
-        startIcon={draw ? <WDCheckmarkIcon /> : ""}
-      >
-        Draw
-      </WDButton>
-      <WDButton
-        color={pause ? "secondary" : "primary"}
-        onClick={() => toggleVote(Vote.PAUSE)}
-        startIcon={pause ? <WDCheckmarkIcon /> : ""}
-      >
-        Pause
-      </WDButton>
-      <WDButton
-        color={cancel ? "secondary" : "primary"}
-        onClick={() => toggleVote(Vote.CANCEL)}
-        startIcon={cancel ? <WDCheckmarkIcon /> : ""}
-      >
-        Cancel
-      </WDButton>
+      {commendButtons}
     </Stack>
   );
 };
