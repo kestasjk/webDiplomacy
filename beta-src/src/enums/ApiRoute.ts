cee78e39c57f4dbbdaeb1d10f5127cc1a1d14f27
Steve Benedict <sbenedict@fb.com>
2022-03-14 12:29:33 -0700

    additional routes removed

--- beta-src/src/enums/ApiRoute.ts
+++ beta-src/src/enums/ApiRoute.ts
@@ -1,11 +1,6 @@
 enum ApiRoute {
-  GAME_TOGGLEVOTE = "game/togglevote",
   GAME_STATUS = "game/status",
   GAME_OVERVIEW = "game/overview",
-  GAME_ORDERS = "game/orders",
-  GAME_SENDMESSAGE = "game/sendmessage",
-  PLAYERS_CD = "players/cd",
-  PLAYERS_MISSING_ORDERS = "players/missing_orders",
 }
 
 export default ApiRoute;
