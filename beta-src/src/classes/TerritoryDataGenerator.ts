bd012f2826cac4a4f46c6b179956132dc4c90f5f
Bryan Fajardo <brfajardo@fb.com>
2022-03-15 17:07:08 -0600

    Alpha props for generators.

--- beta-src/src/classes/TerritoryDataGenerator.ts
+++ beta-src/src/classes/TerritoryDataGenerator.ts
@@ -3,22 +3,22 @@ import TerritoryEnum from "../enums/Territory";
 import Territories from "../data/Territories";
 
 export default class TerritoryDataGenerator implements Territory {
-  public name: Territory["name"];
-
   public abbr: Territory["abbr"];
+
+  public name: Territory["name"];
 
   public type: Territory["type"];
 
   constructor(terr: TerritoryEnum) {
-    this.name = Territories[terr].name;
     this.abbr = Territories[terr].abbr;
+    this.name = Territories[terr].name;
     this.type = Territories[terr].type;
   }
 
   get territory(): Territory {
     return {
-      name: this.name,
       abbr: this.abbr,
+      name: this.name,
       type: this.type,
     };
   }
