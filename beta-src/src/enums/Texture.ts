7b2085d7e039fb8301e44c5a795de3a35eedf494
shelbycodazen <scollopy@codazen.com>
2022-03-25 13:48:43 -0700

    add country textures

--- beta-src/src/enums/Texture.ts
+++ beta-src/src/enums/Texture.ts
@@ -1,7 +1,14 @@
 enum Texture {
+  AUSTRIA = "url(#austria)",
+  ENGLAND = "url(#england)",
+  FRANCE = "url(#france)",
+  GERMANY = "url(#germany)",
+  ITALY = "url(#italy)",
   LAND = "#F9F2E6",
-  SEA = "#96B2B2",
   NEUTRAL_LAND = "#E5CFBE",
+  RUSSIA = "url(#russia)",
+  SEA = "#96B2B2",
+  TURKEY = "url(#turkey)",
 }
 
 export default Texture;
