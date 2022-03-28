3d59c655912eebe4dd132bac144197be524975c7
Steve Benedict <sbenedict@fb.com>
2022-03-28 10:55:49 -0700

    updated interfaces for possible responses

--- beta-src/src/state/interfaces/GameDataResponse.ts
+++ beta-src/src/state/interfaces/GameDataResponse.ts
@@ -1,14 +1,30 @@
 import ContextVar from "../../interfaces/ContextVar";
 import Order from "../../interfaces/Order";
+import { ITerritory, ITerrStatus, IUnit } from "../../models/Interfaces";
 
+interface CountryID {
+  contextVars: ContextVar;
+  currentOrders: Order[];
+}
+
+interface InvalidCountryID {
+  countryID: number;
+}
+
+interface GameData {
+  territories: {
+    [key: string]: ITerritory;
+  };
+  units: {
+    [key: string]: IUnit;
+  };
+  territoryStatuses: ITerrStatus[];
+}
 interface GameDataResponse {
   msg: string;
   referenceCode: string;
   success: boolean;
-  data: {
-    contextVars: ContextVar;
-    currentOrders: Order[];
-  };
+  data: CountryID | InvalidCountryID | GameData;
 }
 
 export default GameDataResponse;
