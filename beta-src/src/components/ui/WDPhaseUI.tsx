a44118d5370f2227a6127f1d25e65524b481837f
Jin Choi <jinchoi1@fb.com>
2022-03-28 17:07:45 -0700

    updated with process time

--- beta-src/src/components/ui/WDPhaseUI.tsx
+++ beta-src/src/components/ui/WDPhaseUI.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import { gameOverview } from "../../state/game/game-api-slice";
 import { useAppSelector } from "../../state/hooks";
-import convertPhaseMin from "../../utils/convertPhaseMin";
+import GameOverviewResponse from "../../state/interfaces/GameOverviewResponse";
 import Season from "../../enums/Season";
 import UIState from "../../enums/UIState";
 import WDCountdownPill from "./WDCountdownPill";
@@ -13,12 +13,12 @@ import WDPillScroller from "./WDPillScroller";
 const WDPhaseUI: React.FC = function (): React.ReactElement {
   const [currentSeason, setCurrentSeason] = useState<Season>(Season.SPRING);
   const [currentYear, setCurrentYear] = useState<number>(1901);
+  const [currentProcessTime, setCurrentProcessTime] =
+    useState<GameOverviewResponse["processTime"]>(null);
   const [timerDisplayState, setTimerDisplayState] = useState(true);
 
-  const { phaseMinutes, season, year } = useAppSelector(gameOverview);
-
-  const phaseTime = phaseMinutes * 60;
-  const endTime = convertPhaseMin(phaseMinutes);
+  const { phaseMinutes, processTime, season, year } =
+    useAppSelector(gameOverview);
 
   const showPillTimer = () => {
     setTimerDisplayState(!timerDisplayState);
@@ -27,7 +27,8 @@ const WDPhaseUI: React.FC = function (): React.ReactElement {
   useEffect(() => {
     setCurrentSeason(season as Season);
     setCurrentYear(year);
-  }, [season, year]);
+    setCurrentProcessTime(processTime);
+  }, [processTime, season, year]);
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -36,9 +37,13 @@ const WDPhaseUI: React.FC = function (): React.ReactElement {
         onClick={showPillTimer}
         year={currentYear}
       />
-      {timerDisplayState ? (
-        <WDCountdownPill endTime={endTime} phaseTime={phaseTime} />
-      ) : (
+      {timerDisplayState && currentProcessTime && (
+        <WDCountdownPill
+          endTime={currentProcessTime}
+          phaseTime={phaseMinutes}
+        />
+      )}
+      {!timerDisplayState && (
         <WDPillScroller season={currentSeason} year={currentYear} />
       )}
     </Box>
--- beta-src/src/utils/convertPhaseMin.ts
+++
@@ -1,4 +0,0 @@
-export default function convertPhaseMin(phaseTime: number) {
-  const endTime = Math.floor(Date.now() / 1000) + phaseTime * 60;
-  return endTime;
-}
