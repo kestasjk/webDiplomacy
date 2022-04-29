ab7f548e1602193c969a9edb14f5583dd36cdaee
Bryan Fajardo <brfajardo@fb.com>
2022-04-29 13:24:21 -0600

    Update based on CR

--- beta-src/src/utils/map/WDArrowMarkerDefs.tsx
+++ beta-src/src/utils/map/WDArrowMarkerDefs.tsx
@@ -11,23 +11,21 @@ const WDArrowMarkerColors = function (
       return (
         <>
           {Object.entries(webDiplomacyTheme.palette.arrowColors).map(
-            ([arrowColor, config]) => {
-              return (
-                <marker
-                  id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
-                  markerWidth={8}
-                  markerHeight={8}
-                  refX="2%"
-                  refY={4}
-                  orient="auto"
-                >
-                  <polygon
-                    points="0 0, 8 4, 0 8, 0 7, 6 4, 0 1"
-                    fill={config.main}
-                  />
-                </marker>
-              );
-            },
+            ([arrowColor, config]) => (
+              <marker
+                id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
+                markerWidth={8}
+                markerHeight={8}
+                refX="2%"
+                refY={4}
+                orient="auto"
+              >
+                <polygon
+                  points="0 0, 8 4, 0 8, 0 7, 6 4, 0 1"
+                  fill={config.main}
+                />
+              </marker>
+            ),
           )}
         </>
       );
@@ -35,20 +33,18 @@ const WDArrowMarkerColors = function (
       return (
         <>
           {Object.entries(webDiplomacyTheme.palette.arrowColors).map(
-            ([arrowColor, config]) => {
-              return (
-                <marker
-                  id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
-                  markerWidth={8}
-                  markerHeight={8}
-                  refX="2%"
-                  refY={4}
-                  orient="auto"
-                >
-                  <polygon points="0 0, 0 8, 1 8, 1 0" fill={config.main} />
-                </marker>
-              );
-            },
+            ([arrowColor, config]) => (
+              <marker
+                id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
+                markerWidth={8}
+                markerHeight={8}
+                refX="2%"
+                refY={4}
+                orient="auto"
+              >
+                <polygon points="0 0, 0 8, 1 8, 1 0" fill={config.main} />
+              </marker>
+            ),
           )}
         </>
       );
@@ -56,25 +52,23 @@ const WDArrowMarkerColors = function (
       return (
         <>
           {Object.entries(webDiplomacyTheme.palette.arrowColors).map(
-            ([arrowColor, config]) => {
-              return (
-                <marker
-                  id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
-                  markerWidth={8}
-                  markerHeight={8}
-                  refX={
-                    (arrowColor as unknown as number) ===
-                    ArrowColor.IMPLIED_FOREIGN
-                      ? 0
-                      : 7.1
-                  }
-                  refY={4}
-                  orient="auto"
-                >
-                  <polygon points="0 0, 8 4, 0 8" fill={config.main} />
-                </marker>
-              );
-            },
+            ([arrowColor, config]) => (
+              <marker
+                id={`arrowHead__${ArrowType[arrowType]}_${ArrowColor[arrowColor]}`}
+                markerWidth={8}
+                markerHeight={8}
+                refX={
+                  (arrowColor as unknown as number) ===
+                  ArrowColor.IMPLIED_FOREIGN
+                    ? 0
+                    : 7.1
+                }
+                refY={4}
+                orient="auto"
+              >
+                <polygon points="0 0, 8 4, 0 8" fill={config.main} />
+              </marker>
+            ),
           )}
         </>
       );
