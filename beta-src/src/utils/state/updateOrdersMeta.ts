5aa3547dd3e71b94915408d68af077afd5fe144a
Bryan Fajardo <brfajardo@fb.com>
2022-05-17 12:43:15 -0600

    Removed code causing bug.

--- beta-src/src/utils/state/updateOrdersMeta.ts
+++ beta-src/src/utils/state/updateOrdersMeta.ts
@@ -3,14 +3,11 @@ import drawOrders from "../map/drawOrders";
 
 /* eslint-disable no-param-reassign */
 export default function updateOrdersMeta(state, updates: EditOrderMeta): void {
-  const entries = Object.entries(updates);
-  if (entries.length) {
-    entries.forEach(([orderID, update]) => {
-      state.ordersMeta[orderID] = {
-        ...state.ordersMeta[orderID],
-        ...update,
-      };
-    });
-    drawOrders(state);
-  }
+  Object.entries(updates).forEach(([orderID, update]) => {
+    state.ordersMeta[orderID] = {
+      ...state.ordersMeta[orderID],
+      ...update,
+    };
+  });
+  drawOrders(state);
 }
