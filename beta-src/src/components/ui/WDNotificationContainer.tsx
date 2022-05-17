2d38abbb060ee75e0aefae415cd88d43339f97e5
Justin Yoo <yoojustin@fb.com>
2022-05-17 11:14:33 -0700

    fixed bug in WDNotificationContainer. Trying to read message of specific index throws errors because writeNotifications is slower than rendering the UI. Changing to destructure the whole index instead of a property of the object at the index fixes the issue

--- beta-src/src/components/ui/WDNotificationContainer.tsx
+++ beta-src/src/components/ui/WDNotificationContainer.tsx
@@ -8,8 +8,8 @@ const WDNotificationContainer: React.FC = function (): React.ReactElement {
   const notifications = useAppSelector(gameNotifications);
   // Notifications[0] will have a value if there player is in build phase and needs to destroy units
   // Currently, indexes 0 and 1 are being used for delete ui purposes but index 1 may or may not be
-  // in use depending on the current state of orders.
-  const { message: deleteTracker } = notifications[0];
+  // in use depending on the current state of orders
+  const { 0: deleteTracker } = notifications;
   return (
     <Stack direction="column">
       {deleteTracker ? <WDDeletePanel notifications={notifications} /> : null}
