719d7638433cbd9b2de415baf996913704b60066
Adam Lerer <alerer@fb.com>
2022-06-02 13:27:22 -0700

    merge

--- beta-src/src/state/game/game-api-slice.ts
+++ beta-src/src/state/game/game-api-slice.ts
@@ -258,13 +258,11 @@ const gameApiSlice = createSlice({
       })
       // fetchGameOverview
       .addCase(fetchGameOverview.pending, (state) => {
-        console.log("fetchGameOverview pending");
         state.outstandingOverviewRequests = true;
         state.apiStatus = "loading";
       })
       .addCase(fetchGameOverview.fulfilled, fetchGameOverviewFulfilled)
       .addCase(fetchGameOverview.rejected, (state, action) => {
-        console.log("fetchGameOverview rejected");
         state.apiStatus = "failed";
         state.error = action.error.message;
         state.outstandingOverviewRequests = false;
