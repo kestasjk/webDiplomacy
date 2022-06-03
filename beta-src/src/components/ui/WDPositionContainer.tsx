281e0c2eeba3c3dd93c63b780f451adbb79389d4
David Wu <dwu@fb.com>
2022-06-03 14:22:33 -0700

    Fix invisible boxes blocking clicks

--- beta-src/src/components/ui/WDPositionContainer.tsx
+++ beta-src/src/components/ui/WDPositionContainer.tsx
@@ -45,6 +45,7 @@ const WDPositionContainer: React.FC<WDPositionContainerProps> = function ({
         touchAction: "none",
         position: "absolute",
         zIndex: Z_INDEX,
+        pointerEvents: "none", // this component is for layout alone, it shouldn't mask out clicks behind it
         ...placement,
       }}
     >
