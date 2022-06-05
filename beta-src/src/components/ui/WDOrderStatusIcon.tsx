ff6deea50afadf5c50408f6fb5cb54635690e4da
Adam Lerer <alerer@fb.com>
2022-06-05 10:31:56 -0700

    align icon

--- beta-src/src/components/ui/WDOrderStatusIcon.tsx
+++ beta-src/src/components/ui/WDOrderStatusIcon.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { Box } from "@mui/material";
+
 import { Lock, PriorityHigh } from "@mui/icons-material";
 import { OrderStatus } from "../../interfaces";
 import WDCheckmarkIcon from "./icons/WDCheckmarkIcon";
@@ -11,13 +13,15 @@ const WDOrderStatusIcon: React.FC<WDOrderStatusIconProps> = function ({
   orderStatus,
 }): React.ReactElement {
   console.log({ orderStatus });
+  let inner = <Box />;
   if (orderStatus.Hidden) {
-    return <Lock sx={{ fontSize: "16px", color: "#666" }} />;
+    inner = <Lock sx={{ fontSize: "16px", color: "#666" }} />;
+  } else if (orderStatus.Saved || orderStatus.Ready) {
+    inner = <WDCheckmarkIcon color={(orderStatus.Ready && "#0A0") || "#888"} />;
+  } else {
+    inner = <PriorityHigh sx={{ fontSize: "16px", color: "#A00" }} />;
   }
-  if (orderStatus.Saved || orderStatus.Ready) {
-    return <WDCheckmarkIcon color={(orderStatus.Ready && "#0A0") || "#888"} />;
-  }
-  return <PriorityHigh sx={{ fontSize: "16px", color: "#A00" }} />;
+  return <Box sx={{ alignIterms: "center" }}>{inner}</Box>;
 };
 
 export default WDOrderStatusIcon;
