d2cf8bffef3629a4d7c7a554377e64d295a940b3
Cesar <cesar@gutierrez.pe>
2022-08-02 22:47:30 -0400

    popover animation

--- beta-src/src/components/ui/WDPopover.tsx
+++ beta-src/src/components/ui/WDPopover.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { motion } from "framer-motion";
 import { useWindowSize } from "react-use";
 
 interface WDPopoverProps {
@@ -12,42 +13,59 @@ const WDPopover: React.FC<WDPopoverProps> = function ({
 }): React.ReactElement {
   const { width, height } = useWindowSize();
 
+  const variants = {
+    showLayover: {
+      opacity: 1,
+      y: 0,
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    hideLayover: {
+      y: width < 500 ? 1500 : 0,
+      x: width > 500 ? 1500 : 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-      {isOpen && (
+    <motion.div
+      className="fixed"
+      style={{
+        bottom: width < 500 ? 0 : 70,
+        left: width < 500 ? 0 : "unset",
+        right: width > 500 ? 100 : "unset",
+        y: width < 500 ? 1500 : "unset",
+        x: width > 500 ? 1500 : "unset",
+      }}
+      animate={isOpen ? "showLayover" : "hideLayover"}
+      variants={variants}
+    >
+      <div>
         <div
-          className="fixed"
+          className="relative"
           style={{
-            bottom: width < 500 ? 0 : 70,
-            left: width < 500 ? 0 : "unset",
-            right: width > 500 ? 100 : "unset",
+            width: width < 500 ? width : 400,
           }}
         >
-          <div>
-            <div
-              className="relative"
-              style={{
-                width: width < 500 ? width : 400,
-              }}
-            >
-              <div
-                className="bg-white m-0 sm:rounded-lg overflow-x-hidden overflow-y-scroll py-4"
-                style={{
-                  minHeight: 264,
-                  maxHeight: height - 32,
-                }}
-              >
-                {children}
-                {width > 500 && (
-                  <div className="absolute bottom-20 right-[-12px] w-6 h-6 bg-white rotate-45" />
-                )}
-              </div>
-            </div>
+          <div
+            className="bg-white m-0 sm:rounded-lg overflow-x-hidden overflow-y-scroll py-4"
+            style={{
+              minHeight: 264,
+              maxHeight: height - 32,
+            }}
+          >
+            {children}
+            {width > 500 && (
+              <div className="absolute bottom-20 right-[-12px] w-6 h-6 bg-white rotate-45" />
+            )}
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </motion.div>
   );
 };
 
