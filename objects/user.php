411c1a5c0b164f2a994b450f77f88b8d8d38eaf5
Kestas Kuliukas <kestas@kuliukas.com>
2022-11-19 12:18:59 +0800

    Flip logic that filters out no account users

--- objects/user.php
+++ objects/user.php
@@ -911,7 +911,7 @@ class User {
 			$ip = dechex($ip);
 		}
 
-		if( !isset($_SESSION['auid']) && $this->id > 1 && !defined('PLAYNOW') && strstr($this->username,'diplonow_') !== false )
+		if( !isset($_SESSION['auid']) && $this->id > 1 && !defined('PLAYNOW') && strstr($this->username,'diplonow_') === false )
 		{
 			// Only store a session hit if we are not impersonating a user
 			$DB->sql_put("INSERT INTO wD_Sessions (userID, lastRequest, hits, ip, userAgent, cookieCode, browserFingerprint)
