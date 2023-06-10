ed7146bbdfe3a4d70c60244d9317e79c128a2af5
Kestas Kuliukas <kestas@kuliukas.com>
2023-06-10 12:09:03 +0800

    Typo fix for mod forum redirect

--- lib/group.php
+++ lib/group.php
@@ -79,7 +79,7 @@ class libGroup
             if( list($viewthreadID) = $DB->sql_row("SELECT id FROM wD_ModForumMessages WHERE type = 'ThreadStart' AND assigned = " . $User->id. " AND isModRead = 0 LIMIT 1" ) )
             {
                 header('refresh: 3; url=modforum.php?viewthread='.$viewthreadID.'#'.$viewthreadID);
-                libHTML::notice("Redirecting to moderator forum", "A moderator forum message assigned to you has been responded to; redirecting you to <a href='modforum.php?viewthread=".$viewthreadID."#".$viewthreadID.">the moderator forum</a> now. Thank you!");
+                libHTML::notice("Redirecting to moderator forum", "A moderator forum message assigned to you has been responded to; redirecting you to <a href='modforum.php?viewthread=".$viewthreadID."#".$viewthreadID."'>the moderator forum</a> now. Thank you!");
             }
         }
         
@@ -88,7 +88,7 @@ class libGroup
             if( list($viewthreadID) = $DB->sql_row("SELECT id FROM wD_ModForumMessages WHERE type = 'ThreadStart' AND fromUserID = " . $User->id. " AND (isUserRead = 0 OR (isUserReplied = 0 AND isUserMustReply = 1)) LIMIT 1" ) )
             {
                 header('refresh: 3; url=modforum.php?viewthread='.$viewthreadID.'#'.$viewthreadID);
-                libHTML::notice("Redirecting to moderator forum", "A moderator forum message has been responded to or requires a response; redirecting you to <a href='modforum.php?viewthread=".$viewthreadID."#".$viewthreadID.">the moderator forum</a> now. Thank you!");
+                libHTML::notice("Redirecting to moderator forum", "A moderator forum message has been responded to or requires a response; redirecting you to <a href='modforum.php?viewthread=".$viewthreadID."#".$viewthreadID."'>the moderator forum</a> now. Thank you!");
             }
         }
     }
