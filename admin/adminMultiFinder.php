9222d438f60f45274e430509bab55baa8d78fb96
Kestas Kuliukas <kestas.j.k@gmail.com>
2023-11-06 17:34:47 +0800

    Fix for multi finder history IP info

--- admin/adminMultiFinder.php
+++ admin/adminMultiFinder.php
@@ -420,24 +420,27 @@ class adminMultiCheck
 		{
 			$tabl = $DB->sql_tabl(
 				"SELECT UNIX_TIMESTAMP(a.lastRequest) as lastRequest, a.userID, u.username,
-					a.hits, a.cookieCode, concat('<a target=\"_blank\" href=\"https://whatismyipaddress.com/ip/',INET_NTOA(a.ip),'\">',INET_NTOA(a.ip),'</a>') as ip, HEX(a.userAgent) as userAgent
+					a.hits, HEX(a.cookieCode) as cookieCode, concat('<a target=\"_blank\" href=\"https://whatismyipaddress.com/ip/',i.ip,'\">',i.ip,'</a>') as ip, HEX(a.userAgent) as userAgent
 				FROM wD_AccessLog a
  				INNER JOIN wD_Users u ON ( u.id = a.userID )
 				INNER JOIN wD_Members m ON ( a.userID = m.userID )
+				LEFT JOIN wD_IPLookups i ON i.ipCode = a.ip
 				WHERE
 					m.gameID IN (".implode(',', $this->aLogsData['activeGameIDs']).")
 					AND a.userID IN ( ".implode(',',$userIDs) .")
-				ORDER BY a.lastRequest DESC"
+				ORDER BY a.lastRequest DESC LIMIT 1000"
 			);
 		}
 		else
 		{
 			$tabl = $DB->sql_tabl(
 				"SELECT UNIX_TIMESTAMP(a.lastRequest) as lastRequest, a.userID, u.username,
-					a.hits, a.cookieCode, concat('<a target=\"_blank\" href=\"https://whatismyipaddress.com/ip/',INET_NTOA(a.ip),'\">',INET_NTOA(a.ip),'</a>') as ip, HEX(a.userAgent) as userAgent
-				FROM wD_AccessLog a INNER JOIN wD_Users u ON ( u.id = a.userID )
+					a.hits, HEX(a.cookieCode) as cookieCode, concat('<a target=\"_blank\" href=\"https://whatismyipaddress.com/ip/',i.ip,'\">',i.ip,'</a>') as ip, HEX(a.userAgent) as userAgent
+				FROM wD_AccessLog a 
+				INNER JOIN wD_Users u ON ( u.id = a.userID )
+				LEFT JOIN wD_IPLookups i ON i.ipCode = a.ip
 				WHERE a.userID IN ( ".implode(',',$userIDs) .")
-				ORDER BY a.lastRequest DESC"
+				ORDER BY a.lastRequest DESC LIMIT 1000"
 			);
 		}
 
