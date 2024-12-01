1ce3116db6606c8c1801ffc5411c19e093523b9b
Kestas "Chris" Kuliukas <kestas.j.k@gmail.com>
2024-12-01 17:19:45 +0800

    Prevent sandbox games limiting play.webdiplomacy.net games

--- botgamecreate.php
+++ botgamecreate.php
@@ -63,7 +63,7 @@ if ($userBotGameCount > 2 || (defined('PLAYNOW') && $userBotGameCount > 0))
 // Limit the number of simultaneous play now / anonymous bot games to 60
 if( defined('PLAYNOW') )
 {
-	list($nopressBotGameCount) = $DB->sql_row("SELECT COUNT(DISTINCT g.id) FROM wD_Games g INNER JOIN wD_Members m ON m.gameID = g.id INNER JOIN wD_Users u ON u.id = m.userID WHERE NOT u.type LIKE '%Bot%' AND g.gameOver = 'No' AND g.playerTypes = 'MemberVsBots' AND u.username LIKE 'diplonow_%'");
+	list($nopressBotGameCount) = $DB->sql_row("SELECT COUNT(DISTINCT g.id) FROM wD_Games g INNER JOIN wD_Members m ON m.gameID = g.id INNER JOIN wD_Users u ON u.id = m.userID WHERE NOT u.type LIKE '%Bot%' AND g.gameOver = 'No' AND g.playerTypes = 'MemberVsBots' AND u.username LIKE 'diplonow_%' AND NOT g.name LIKE 'SB_%'");
 	if( $nopressBotGameCount > 199 )
 	{
 		libHTML::notice(l_t('Anonymous bot game limit reached'), l_t('Anonymous bot game limit '.$nopressBotGameCount.'/100 reached: Apologies, the anonymous bot game limit has been reached. To conserve server resources we have to limit the number of anonymous games. Please try again later, or create an account on the community page.'));
